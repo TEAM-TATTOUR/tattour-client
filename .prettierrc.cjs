ddd0313bdbe8cb7728b2481f051544ce9826f398
gunom <gu3062@naver.com>
2023-06-30 17:43:13 +0900

    chore: husky 적용

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    bracketSpacing: true,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: "all",
+    printWidth: 100,
+    endOfLine: "auto",
+    useTabs: false,
+    semi: true,
+    jsxSingleQuote: true,
+    arrowParens: "always",
+};
