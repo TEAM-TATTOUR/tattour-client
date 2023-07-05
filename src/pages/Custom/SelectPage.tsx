ac1f42885481618d41294a1f7d81be8d3201d07c
Yeonseo-Jo <jys073190@gmail.com>
2023-07-06 04:33:44 +0900

    feat : UI 구현

--- src/pages/Custom/SelectPage.tsx
+++ src/pages/Custom/SelectPage.tsx
@@ -4,22 +4,19 @@ const SelectPage = () => {
   return (
     <St.SelectWrapper>
       <St.SelectInfoContainer>
-        <St.InfoMainText>안녕하세요! 어떤 상황에 놓여있나요?</St.InfoMainText>
-        <St.InfoSubText>상황에 맞게 제작해드려요</St.InfoSubText>
+        <St.InfoMainText>어떤 상황에 놓여있나요?</St.InfoMainText>
+        <St.InfoSubText>상황에 맞게 제작해 드려요</St.InfoSubText>
       </St.SelectInfoContainer>
 
       <St.SelectBtnContainer>
         <St.SelectBtnContent>
-          <a href='/'>
-            <St.SelectBtnTitle>이미 그려 둔 도안이 있어요</St.SelectBtnTitle>
-            <St.SelectBtnDetail>이미지 파일을 그대로 제작 해드릴게요</St.SelectBtnDetail>
-          </a>
+          <St.SelectBtnTitle type='exist'>이미 그려 둔 도안이 있어요</St.SelectBtnTitle>
+          <St.SelectBtnDetail type='exist'>이미지 파일을 그대로 제작 해드릴게요</St.SelectBtnDetail>
         </St.SelectBtnContent>
+
         <St.SelectBtnContent>
-          <a href='/'>
-            <St.SelectBtnTitle>커스텀 도안을 제작하고 싶어요</St.SelectBtnTitle>
-            <St.SelectBtnDetail>신청서에 맞게 세심하게 제작해드릴게요</St.SelectBtnDetail>
-          </a>
+          <St.SelectBtnTitle type='make'>커스텀 도안을 제작하고 싶어요</St.SelectBtnTitle>
+          <St.SelectBtnDetail type='make'>신청서에 맞게 세심하게 제작해드릴게요</St.SelectBtnDetail>
         </St.SelectBtnContent>
       </St.SelectBtnContainer>
     </St.SelectWrapper>
@@ -39,17 +36,74 @@ const St = {
     height: 100vh;
   `,
 
-  SelectInfoContainer: styled.section``,
+  SelectInfoContainer: styled.article`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-  InfoMainText: styled.h2``,
+    margin-bottom: 4rem;
 
-  InfoSubText: styled.p``,
+    gap: 1.2rem;
+  `,
 
-  SelectBtnContainer: styled.section``,
+  InfoMainText: styled.h2`
+    color: ${({ theme }) => theme.colors.gray8};
 
-  SelectBtnContent: styled.article``,
+    ${({ theme }) => theme.fonts.title_semibold_20};
+  `,
 
-  SelectBtnTitle: styled.h3``,
+  InfoSubText: styled.p`
+    color: ${({ theme }) => theme.colors.gray3};
 
-  SelectBtnDetail: styled.p``,
+    ${({ theme }) => theme.fonts.body_medium_14};
+  `,
+
+  SelectBtnContainer: styled.article`
+    display: flex;
+    gap: 1.5rem;
+
+    & > a {
+      text-decoration: none;
+    }
+  `,
+
+  SelectBtnContent: styled.button`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 0.8rem;
+
+    width: 16rem;
+    height: 16.8rem;
+    padding: 0 2.7rem;
+
+    background-color: ${({ theme }) => theme.colors.bg};
+
+    border-radius: 0.5rem;
+
+    cursor: pointer;
+  `,
+
+  SelectBtnTitle: styled.h3<{ type: string }>`
+    width: ${({ type }) => (type === 'exist' ? 8.6 : 10)}rem;
+
+    color: ${({ theme }) => theme.colors.gray7};
+
+    ${({ theme }) => theme.fonts.title_semibold_16};
+
+    text-align: center;
+    white-space: pre-line;
+  `,
+
+  SelectBtnDetail: styled.p<{ type: string }>`
+    width: ${({ type }) => (type === 'exist' ? 10.6 : 11.6)}rem;
+
+    color: ${({ theme }) => theme.colors.gray3};
+
+    ${({ theme }) => theme.fonts.detail_medium_12};
+
+    text-align: center;
+    white-space: pre-line;
+  `,
 };
