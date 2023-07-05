a6ffb8d0cb60ee9ef3ff197ca97df7928a9ae614
seoAreum <1971236@hansung.ac.kr>
2023-07-06 04:29:05 +0900

    feat: 로그인 뷰 푸터 구현

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,5 +1,6 @@
 import { styled } from 'styled-components';
 import logo from '../assets/logo.png';
+import kakao from '../assets/logo_kakao.png';
 
 function LoginPage() {
   return (
@@ -9,6 +10,11 @@ function LoginPage() {
         <St.Title>후회없는 선택의 여정을 함께,</St.Title>
         <St.SubTitle>1초만에 가입하고 지금만 받을 수 있는 혜택 받아가세요!</St.SubTitle>
       </St.ContentsWrapper>
+
+      <St.LoginFooter>
+        <img src={kakao} />
+        <St.FooterText>카카오로 계속하기</St.FooterText>
+      </St.LoginFooter>
     </St.LoginWrapper>
   );
 }
@@ -16,6 +22,7 @@ function LoginPage() {
 const St = {
   LoginWrapper: styled.section`
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
 
@@ -45,6 +52,26 @@ const St = {
     color: ${({ theme }) => theme.colors.gray1};
 
     ${({ theme }) => theme.fonts.body_medium_14};
+  `,
+
+  LoginFooter: styled.footer`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    bottom: 0;
+
+    width: 100%;
+    height: 7rem;
+
+    background-color: #fee500;
+
+    gap: 1rem;
+  `,
+
+  FooterText: styled.p`
+    // 임의지정 -> 스타일 가이드 나오면 바꾸기!!
+    ${({ theme }) => theme.fonts.title_semibold_18};
   `,
 };
 
