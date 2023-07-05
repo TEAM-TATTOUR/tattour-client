d4b8fb3311526cb3ef8f2b30c8c19bf4ad048612
lydiacho <sheecho0804@ewhain.net>
2023-07-06 03:45:23 +0900

    style: hr 구분선 추가

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,12 +1,23 @@
 import TattooList from "../components/List/TattooList";
+import styled from "styled-components";
 
 const ListPage = () => {
   return (
     <div>
         ListPage
+        <St.Line/>
         <TattooList/>
     </div>
   )
 }
 
 export default ListPage
+
+const St = {
+    Line : styled.hr`
+        height: 1.3rem;
+
+        background-color: ${({ theme }) => theme.colors.bg};
+        border-width: 0rem;
+    `
+}
