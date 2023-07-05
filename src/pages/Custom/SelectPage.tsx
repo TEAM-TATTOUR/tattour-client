873b6ff88e3842295a75114d73a5c8a242b2bc77
Yeonseo-Jo <jys073190@gmail.com>
2023-07-06 03:01:56 +0900

    feat : 기본 구조

--- src/pages/Custom/SelectPage.tsx
+++ src/pages/Custom/SelectPage.tsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import { styled } from 'styled-components';
 
 const SelectPage = () => {
-  return <div>골라골라~</div>;
+  return (
+    <St.SelectWrapper>
+      <St.SelectInfoContainer>
+        <St.InfoMainText>안녕하세요! 어떤 상황에 놓여있나요?</St.InfoMainText>
+        <St.InfoSubText>상황에 맞게 제작해드려요</St.InfoSubText>
+      </St.SelectInfoContainer>
+
+      <St.SelectBtnContainer>
+        <St.SelectBtnContent>
+          <a href='/'>
+            <St.SelectBtnTitle>이미 그려 둔 도안이 있어요</St.SelectBtnTitle>
+            <St.SelectBtnDetail>이미지 파일을 그대로 제작 해드릴게요</St.SelectBtnDetail>
+          </a>
+        </St.SelectBtnContent>
+        <St.SelectBtnContent>
+          <a href='/'>
+            <St.SelectBtnTitle>커스텀 도안을 제작하고 싶어요</St.SelectBtnTitle>
+            <St.SelectBtnDetail>신청서에 맞게 세심하게 제작해드릴게요</St.SelectBtnDetail>
+          </a>
+        </St.SelectBtnContent>
+      </St.SelectBtnContainer>
+    </St.SelectWrapper>
+  );
 };
 
 export default SelectPage;
+
+const St = {
+  SelectWrapper: styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    height: 100vh;
+  `,
+
+  SelectInfoContainer: styled.section``,
+
+  InfoMainText: styled.h2``,
+
+  InfoSubText: styled.p``,
+
+  SelectBtnContainer: styled.section``,
+
+  SelectBtnContent: styled.article``,
+
+  SelectBtnTitle: styled.h3``,
+
+  SelectBtnDetail: styled.p``,
+};
