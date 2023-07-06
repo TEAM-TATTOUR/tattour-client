6745abe78a79dcbd97e1572f4544f96b23215727
Yeonseo-Jo <jys073190@gmail.com>
2023-07-07 01:23:50 +0900

    feat : 캐러셀 기본 구조

--- src/components/Custom/OnBoardingCarousel.tsx
+++ src/components/Custom/OnBoardingCarousel.tsx
@@ -1,5 +1,36 @@
+import { styled } from 'styled-components';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 const OnBoardingCarousel = () => {
-  return <div>샥샥~</div>;
+  return (
+    <>
+      <St.Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        navigation
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide </SwiperSlide>
+      </St.Swiper>
+    </>
+  );
 };
 
 export default OnBoardingCarousel;
+
+const St = {
+  Swiper: styled(Swiper)`
+    .swiper-slide {
+      height: 63.6rem;
+    }
+  `,
+};
