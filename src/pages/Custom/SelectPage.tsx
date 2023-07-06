4b236a6f517d5b88a79d44cf02a6afdb2969e790
Yeonseo-Jo <jys073190@gmail.com>
2023-07-06 14:11:59 +0900

    feat : 버튼 둘 중 하나만 활성화 되도록

--- src/pages/Custom/SelectPage.tsx
+++ src/pages/Custom/SelectPage.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { styled } from 'styled-components';
 
 const SelectPage = () => {
+  const [activeBtn, setActiveBtn] = useState('');
+  const btnRef = useRef<HTMLButtonElement[]>([]);
+
   const handleClickSelBtn = (e: React.MouseEvent<HTMLButtonElement>) => {
     const target = e.target as HTMLElement;
-    console.log(target);
-    target.classList.toggle('isClicked');
+    setActiveBtn(target.id);
   };
+
+  useEffect(() => {
+    btnRef.current.forEach((ref) => {
+      console.log(activeBtn, '??');
+      if (ref.id === activeBtn) {
+        ref.classList.add('isClicked');
+      } else {
+        ref.classList.remove('isClicked');
+      }
+    });
+  }, [activeBtn]);
+
+  console.log(activeBtn, '!!!');
 
   return (
     <St.SelectWrapper>
@@ -16,14 +31,24 @@ const SelectPage = () => {
       </St.SelectInfoContainer>
 
       <St.SelectBtnContainer>
-        <St.SelectBtnContent type='button' id='exist' onClick={handleClickSelBtn}>
+        <St.SelectBtnContent
+          type='button'
+          id='exist'
+          ref={(element: HTMLButtonElement) => (btnRef.current[0] = element)}
+          onClick={handleClickSelBtn}
+        >
           <St.SelectBtnTitle $case='exist'>이미 그려 둔 도안이 있어요</St.SelectBtnTitle>
           <St.SelectBtnDetail $case='exist'>
             이미지 파일을 그대로 제작 해드릴게요
           </St.SelectBtnDetail>
         </St.SelectBtnContent>
 
-        <St.SelectBtnContent type='button' id='make' onClick={handleClickSelBtn}>
+        <St.SelectBtnContent
+          type='button'
+          id='make'
+          ref={(element: HTMLButtonElement) => (btnRef.current[1] = element)}
+          onClick={handleClickSelBtn}
+        >
           <St.SelectBtnTitle $case='make'>커스텀 도안을 제작하고 싶어요</St.SelectBtnTitle>
           <St.SelectBtnDetail $case='make'>
             신청서에 맞게 세심하게 제작해드릴게요
