e06500043fedbe29818c0cd5c14cac939e679201
lydiacho <sheecho0804@ewhain.net>
2023-07-07 00:26:56 +0900

    feat : 하단 list 카드와 전체 배열 구현

--- src/components/List/TattooList.tsx
+++ src/components/List/TattooList.tsx
@@ -18,6 +18,26 @@ const TattooList = () => {
         ))}
       </St.BtnContainer>
       <St.CountText>전체 {count}개</St.CountText>
+      <St.CardContainer>
+          <St.Card>
+            <St.CardImg></St.CardImg>
+            <h2>고양이 리본 타투</h2>
+            <p>4000원</p>
+            <div>
+              <St.CardDiscount>25%</St.CardDiscount>
+              <St.CardPrice>2,500원</St.CardPrice>
+            </div>
+          </St.Card>
+          <St.Card>
+            <St.CardImg></St.CardImg>
+            <h2>고양이 리본 타투</h2>
+            <p>4000원</p>
+            <div>
+              <St.CardDiscount>25%</St.CardDiscount>
+              <St.CardPrice>2,500원</St.CardPrice>
+            </div>
+          </St.Card>
+      </St.CardContainer>
     </St.Wrapper>
   )
 }
@@ -55,5 +75,45 @@ const St = {
     margin: 2.8rem 0rem 1.6rem 2.2rem;
     color: ${({ theme }) => theme.colors.gray4};
     ${({ theme }) => theme.fonts.body_medium_14};
+  `,
+  CardContainer: styled.section`
+    display:grid;
+    grid-template-columns: 1fr 1fr;
+  `,
+  Card: styled.article`
+    display:flex;
+    flex-direction: column;
+
+    & > h2 {
+      margin: 1.5rem 0rem 0rem 2rem;
+      color: ${({ theme }) => theme.colors.gray7};
+      ${({ theme }) => theme.fonts.title_semibold_16};
+    }
+
+    & > p {
+      margin: 0.4rem 0rem 0rem 2rem;
+      color: ${({ theme }) => theme.colors.gray1};
+      ${({ theme }) => theme.fonts.body_line_medium_14};
+    }
+
+    & > div {
+      margin: 0.3rem 0rem 0rem 2rem;
+    }
+
+  `,
+  CardImg: styled.i`    // 추후 img로 변경 
+      width: 18.7rem;
+      height: 20.1rem;
+      background-color: ${({ theme }) => theme.colors.gray1};
+  `,
+  CardDiscount: styled.span`
+    color: ${({ theme }) => theme.colors.pink5};
+    ${({ theme }) => theme.fonts.title_semibold_16};
+  `,
+  CardPrice: styled.span`
+    margin-left: 0.5rem;
+
+    color: ${({ theme }) => theme.colors.gray3};
+    ${({ theme }) => theme.fonts.title_semibold_16};
   `
 }
