927079db300ff79465f23cb813d17d1fd6858b93
seoAreum <1971236@hansung.ac.kr>
2023-07-06 23:41:09 +0900

    feat: 이름 입력받는 부분 구현

--- src/components/Register/InputName.tsx
+++ src/components/Register/InputName.tsx
@@ -1,5 +1,21 @@
+import styled from 'styled-components';
+import { TITLE, SUB_TITLE } from '../../constants/TitleInfo';
+import TitleForm from './TitleForm';
+
 const InputName = () => {
-  return <div>InputName</div>;
+  return (
+    <St.InputNameWrapper>
+      <TitleForm title={TITLE[0]} subTitle={SUB_TITLE[0]} />
+    </St.InputNameWrapper>
+  );
+};
+
+const St = {
+  InputNameWrapper: styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `,
 };
 
 export default InputName;
