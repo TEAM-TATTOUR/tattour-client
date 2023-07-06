0faa4cf14ccfbb7b3359c84a8e0ea3d7365bda54
Yeonseo-Jo <jys073190@gmail.com>
2023-07-06 23:54:47 +0900

    feat : 온보딩 페이지 라우팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,14 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import OnBoardingPage from './pages/Custom/OnBoardingPage';
 
 const Router = () => {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/onBoarding' element={<OnBoardingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
---
+++ src/pages/Custom/OnBoardingPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OnBoardingPage = () => {
+  return <div>캐러셀 샥샥</div>;
+};
+
+export default OnBoardingPage;
