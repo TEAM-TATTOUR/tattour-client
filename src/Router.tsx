9005dbea59977881a6a2b9d6680bccfab74fb833
lydiacho <sheecho0804@ewhain.net>
2023-07-06 23:08:02 +0900

    style: header 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,14 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListPage from './pages/ListPage';
 
 const Router = () => {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ListPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
--- src/components/List/TattooList.tsx
+++ src/components/List/TattooList.tsx
@@ -1,8 +1,16 @@
+import styled from "styled-components";
 
 const TattooList = () => {
   return (
-    <div>TattooList</div>
+    <St.Header>ALL</St.Header>
   )
 }
 
 export default TattooList
+
+const St = {
+  Header : styled.h1`
+  margin: 2.8rem 0rem 2.2rem 2rem;
+  ${({ theme }) => theme.fonts.title_semibold_18};  // 추후 유료 font 변경 예정 
+  `
+}
