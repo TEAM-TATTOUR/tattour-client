caf4ff148a54638d56517a1cdd97d81b9cc81091
gunom <gu3062@naver.com>
2023-07-08 02:03:14 +0900

    feat: MainBanner 구현

--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -1,23 +1,31 @@
 import { styled } from 'styled-components';
+import { IcArrowRightDark, ImgHome } from '../assets/icon';
+import { renderToStaticMarkup } from 'react-dom/server';
 
 const MainBanner = () => {
+  const svgString = encodeURIComponent(renderToStaticMarkup(<ImgHome />));
+
   return (
-    <St.BannerWrapper>
+    <St.BannerWrapper svgstring={svgString}>
       <St.CopyWright>
         Journey <br />
         of No Regrets,
       </St.CopyWright>
       <St.SmallCopy>나만의 타투 스티커를 만들어 보세요 </St.SmallCopy>
+      <St.HomeButton type='button'>
+        신청하기
+        <IcArrowRightDark />
+      </St.HomeButton>
     </St.BannerWrapper>
   );
 };
 
 const St = {
-  BannerWrapper: styled.section`
+  BannerWrapper: styled.section<{ svgstring: string }>`
     width: 100%;
     height: 76.1rem;
     padding: 0 1.7rem;
-    background-color: black;
+    background-image: ${({ svgstring }) => `url("data:image/svg+xml,${svgstring}")`};
   `,
 
   CopyWright: styled.p`
@@ -32,6 +40,20 @@ const St = {
 
     ${({ theme }) => theme.fonts.body_medium_16};
     color: ${({ theme }) => theme.colors.white};
+  `,
+
+  HomeButton: styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 1.4rem;
+
+    width: 11.1rem;
+    height: 4.2rem;
+
+    background-color: ${({ theme }) => theme.colors.pink5};
+    font: ${({ theme }) => theme.fonts.title_semibold_20};
+    border-radius: 0.5rem;
   `,
 };
 
