bd139bd3852e589b5b54c30ed21fe08faab9d3ac
Yeonseo-Jo <jys073190@gmail.com>
2023-07-07 14:52:58 +0900

    feat : 이미 그려둔 도안이 있는 상황의 온보딩 캐러셀 슬라이드 데이터 추가

---
+++ src/assets/data/ONBOARDING_CAROUSEL_CONTNET.ts
@@ -0,0 +1,22 @@
+import ImgTestOnBoarding from './ImgTestOnBoarding.png';
+
+export const EXIST_CUSTOM_CAROUSEL_DATA = [
+  {
+    mainText: '크기를 선택해주세요',
+    subText: '고민 중인 실제 사이즈를 선택해보세요!',
+    mockUpImg: ImgTestOnBoarding,
+    imgAlt: '크기 선택 안내 온보딩 이미지',
+  },
+  {
+    mainText: '도안 이미지를 첨부해주세요',
+    subText: '직접 그린 도안을 첨부해주세요',
+    mockUpImg: ImgTestOnBoarding,
+    imgAlt: '도안 이미지 첨부 안내 온보딩 이미지',
+  },
+  {
+    mainText: '커스텀 도안을 공개해보세요',
+    subText: '커스텀 도안을 공개하면 엄청난 할인을 받을 수 있어요',
+    mockUpImg: ImgTestOnBoarding,
+    imgAlt: '커스텀 도안 공개 안내 온보딩 이미지',
+  },
+];
