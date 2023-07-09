82e1623a21bafc196aea520a5be86a933d220529
seoAreum <1971236@hansung.ac.kr>
2023-07-09 21:53:15 +0900

    feat: footer 구현

--- src/components/Welcome/WelcomeFooter.tsx
+++ src/components/Welcome/WelcomeFooter.tsx
@@ -1,9 +1,35 @@
+import { styled } from 'styled-components';
+
 const WelcomeFooter = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <St.LoginFooter>
+      <St.FooterText>홈으로 이동하기</St.FooterText>
+    </St.LoginFooter>
+  );
 };
+
+const St = {
+    LoginFooter: styled.footer`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: absolute;
+      bottom: 0;
+  
+      width: 100%;
+      height: 7rem;
+  
+      background-color: ${({ theme }) => theme.colors.gray9};
+  
+      gap: 1rem;
+    `,
+  
+    FooterText: styled.p`
+      color: ${({ theme }) => theme.colors.white};
+
+      ${({ theme }) => theme.fonts.title_semibold_18};
+    `,
+  };
+  
 
 export default WelcomeFooter;
