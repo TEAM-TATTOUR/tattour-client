871c9953745f299baea0499ba8d654ac46065fd9
seoAreum <1971236@hansung.ac.kr>
2023-07-09 22:10:52 +0900

    chore: 네이밍 수정

--- src/components/Welcome/WelcomeFooter.tsx
+++ src/components/Welcome/WelcomeFooter.tsx
@@ -2,34 +2,33 @@ import { styled } from 'styled-components';
 
 const WelcomeFooter = () => {
   return (
-    <St.LoginFooter>
+    <St.WelcomeFooter>
       <St.FooterText>홈으로 이동하기</St.FooterText>
-    </St.LoginFooter>
+    </St.WelcomeFooter>
   );
 };
 
 const St = {
-    LoginFooter: styled.footer`
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      position: absolute;
-      bottom: 0;
-  
-      width: 100%;
-      height: 7rem;
-  
-      background-color: ${({ theme }) => theme.colors.gray9};
-  
-      gap: 1rem;
-    `,
-  
-    FooterText: styled.p`
-      color: ${({ theme }) => theme.colors.white};
+  WelcomeFooter: styled.footer`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    bottom: 0;
 
-      ${({ theme }) => theme.fonts.title_semibold_18};
-    `,
-  };
-  
+    width: 100%;
+    height: 7rem;
+
+    background-color: ${({ theme }) => theme.colors.gray9};
+
+    gap: 1rem;
+  `,
+
+  FooterText: styled.p`
+    color: ${({ theme }) => theme.colors.white};
+
+    ${({ theme }) => theme.fonts.title_semibold_18};
+  `,
+};
 
 export default WelcomeFooter;
