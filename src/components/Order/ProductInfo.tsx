c585778e89327108fed85e4a783eaf1b2b78c0c0
lydiacho <sheecho0804@ewhain.net>
2023-07-09 17:13:55 +0900

    feat: UI 기본 구조 세팅

--- src/components/Order/ProductInfo.tsx
+++ src/components/Order/ProductInfo.tsx
@@ -2,8 +2,63 @@ import styled from "styled-components";
 
 const ProductInfo = () => {
   return (
-    <div>ProductInfo</div>
+    <St.Wrapper>
+        <St.Image/>
+        <St.InfoContainer>
+            <St.Name>
+
+            </St.Name>
+            <St.Description>
+                <St.OriginalPrice>
+
+                </St.OriginalPrice>
+                <St.ItemPrice>
+
+                </St.ItemPrice>
+                <St.Line>
+
+                </St.Line>
+                <St.Count>
+
+                </St.Count>
+            </St.Description>
+        </St.InfoContainer>
+    </St.Wrapper>
   )
 }
 
 export default ProductInfo
+
+const St = {
+    Wrapper : styled.section`
+        display: flex;
+        flex-direction: column;
+    `,
+    Image : styled.img`
+    
+    `,
+    InfoContainer : styled.article`
+    
+    `, 
+    Name : styled.h1`
+    
+    `,
+    Description : styled.p`
+        
+
+    `,
+    OriginalPrice : styled.span`
+    
+    `,
+    ItemPrice : styled.span`
+    
+    `,
+    Line : styled.div`
+    
+    `,
+    Count : styled.span`
+    
+    `,
+    
+
+}
