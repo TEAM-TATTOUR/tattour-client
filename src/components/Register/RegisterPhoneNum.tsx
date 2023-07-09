42f05f870c6c40f4d9650e4aaf87e87a1b030a6a
seoAreum <1971236@hansung.ac.kr>
2023-07-09 20:50:33 +0900

    chore: 네이밍 변경

--- src/components/Register/RegisterPhoneNum.tsx
+++ src/components/Register/RegisterPhoneNum.tsx
@@ -5,27 +5,27 @@ import { useState } from 'react';
 import RegisterPhoneNumForm from './RegisterPhoneNumForm';
 import InputCertificationForm from './InputCertificationForm';
 
-const InputNum = () => {
+const RegisterPhoneNum = () => {
   // 전화번호 입력 후, 인증 버튼이 클릭되었는지 여부를 확인하기 위한 상태
   const [isVisible, setIsVisible] = useState(false);
   return (
-    <St.InputNumWrapper>
+    <St.RegisterPhoneNumWrapper>
       <TitleForm title={TITLE[1]} subTitle={SUB_TITLE[1]} />
 
       <RegisterPhoneNumForm isVisible={isVisible} setIsVisible={setIsVisible} />
 
       {/* isVisible이 true인 경우에만 인증번호 입력 폼 나옴 */}
       {isVisible && <InputCertificationForm />}
-    </St.InputNumWrapper>
+    </St.RegisterPhoneNumWrapper>
   );
 };
 
 const St = {
-  InputNumWrapper: styled.section`
+  RegisterPhoneNumWrapper: styled.section`
     display: flex;
     flex-direction: column;
     align-items: center;
   `,
 };
 
-export default InputNum;
+export default RegisterPhoneNum;
