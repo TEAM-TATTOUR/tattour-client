c20b0000ae8fe2afdc528c8e07600d1757178517
seoAreum <1971236@hansung.ac.kr>
2023-07-09 21:53:37 +0900

    feat: Welcome 내용 구현

--- src/components/Welcome/WelcomeHome.tsx
+++ src/components/Welcome/WelcomeHome.tsx
@@ -1,9 +1,38 @@
+import { styled } from 'styled-components';
+import { ImgLogoDark } from '../../assets/icon';
+
 const WelcomeHome = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <St.ContentsWrapper>
+      <ImgLogoDark />
+      <St.Title>타투어가 되신 것을 환영해요!</St.Title>
+      <St.SubTitle>후회없는 타투 선택의 여정을 함께 할게요</St.SubTitle>
+    </St.ContentsWrapper>
+  );
+};
+
+const St = {
+  ContentsWrapper: styled.article`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `,
+
+  Title: styled.p`
+    margin-top: 3.2rem;
+
+    color: ${({ theme }) => theme.colors.gray8};
+
+    ${({ theme }) => theme.fonts.title_semibold_20};
+  `,
+
+  SubTitle: styled.p`
+    margin-top: 1.2rem;
+
+    color: ${({ theme }) => theme.colors.gray3};
+
+    ${({ theme }) => theme.fonts.body_medium_14};
+  `,
 };
 
 export default WelcomeHome;
