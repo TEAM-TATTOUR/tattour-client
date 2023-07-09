67cc470f4966b1ca811d551cb3be0ad107132436
Yeonseo-Jo <jys073190@gmail.com>
2023-07-09 23:34:52 +0900

    feat : 버튼 선택 시 스타일 바뀌게

--- src/components/Custom/Common/SelectSizeCustom.tsx
+++ src/components/Custom/Common/SelectSizeCustom.tsx
@@ -1,7 +1,30 @@
 import { styled } from 'styled-components';
 import SelectSizeCustomBtn from './SelectSizeCustomBtn';
+import { useState, useRef, useEffect } from 'react';
 
 const SelectSizeCustom = () => {
+  const [selectedBtn, setSelectedBtn] = useState('');
+  const sizeBtnRef = useRef<HTMLButtonElement[]>([]);
+
+  const handleClickSelBtn = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const target = e.target as HTMLElement;
+    setSelectedBtn(target.id);
+  };
+
+  useEffect(() => {
+    //버튼 하나씩만 누를 수 있도록
+    sizeBtnRef.current.forEach((ref) => {
+      if (ref.id === selectedBtn) {
+        console.log(ref);
+        ref.classList.add('isSelected');
+      } else {
+        ref.classList.remove('isSelected');
+      }
+    });
+  }, [selectedBtn]);
+
+  console.log(selectedBtn, '???');
+
   return (
     <St.SizeWrapper>
       <St.SizeInfoContainer>
@@ -9,10 +32,38 @@ const SelectSizeCustom = () => {
         <St.InfoSubText>고민되는 크기를 선택해주세요!</St.InfoSubText>
       </St.SizeInfoContainer>
       <St.SizeBntContainer>
-        <SelectSizeCustomBtn title='5cm 이하' detail='동전 크기' />
-        <SelectSizeCustomBtn title='A4 1/8' detail='신용카드, 담뱃값 크기' />
-        <SelectSizeCustomBtn title='A4 1/4' detail='엽서, 손바닥 크기' />
-        <SelectSizeCustomBtn title='5cm 이하' detail='동전 크기' />
+        <SelectSizeCustomBtn
+          id='size 1'
+          title='5cm 이하'
+          detail='동전 크기'
+          ref={(element: HTMLButtonElement) => (sizeBtnRef.current[0] = element)}
+          onClick={handleClickSelBtn}
+          selectedBtn={selectedBtn}
+        />
+        <SelectSizeCustomBtn
+          id='size 2'
+          title='A4 1/8'
+          detail='신용카드, 담뱃값 크기'
+          ref={(element: HTMLButtonElement) => (sizeBtnRef.current[1] = element)}
+          onClick={handleClickSelBtn}
+          selectedBtn={selectedBtn}
+        />
+        <SelectSizeCustomBtn
+          id='size 3'
+          title='A4 1/4'
+          detail='엽서, 손바닥 크기'
+          ref={(element: HTMLButtonElement) => (sizeBtnRef.current[2] = element)}
+          onClick={handleClickSelBtn}
+          selectedBtn={selectedBtn}
+        />
+        <SelectSizeCustomBtn
+          id='size 4'
+          title='A4 1/2'
+          detail='아래팔 한 쪽 면 크기'
+          ref={(element: HTMLButtonElement) => (sizeBtnRef.current[3] = element)}
+          onClick={handleClickSelBtn}
+          selectedBtn={selectedBtn}
+        />
       </St.SizeBntContainer>
     </St.SizeWrapper>
   );
--- src/components/Custom/Common/SelectSizeCustomBtn.tsx
+++ src/components/Custom/Common/SelectSizeCustomBtn.tsx
@@ -1,50 +1,79 @@
 import { styled } from 'styled-components';
 
-import { IcApplyCheckLight } from '../../../assets/icon';
+import { IcApplyCheckLight, IcApplyCheckPink } from '../../../assets/icon';
 import img_home from '../../../assets/icon/img_home.svg';
+import { forwardRef } from 'react';
 
 interface SelectSizeCustomBtnProps {
-  title: stirng;
+  id: string;
+  title: string;
   detail: string;
+  selectedBtn: string;
+  // ref: (element: HTMLButtonElement) => HTMLButtonElement;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const SelectSizeCustomBtn = ({ title, detail }: SelectSizeCustomBtnProps) => {
-  return (
-    <St.SizeBtn type='button'>
-      <St.SizeBtnImgBox>
-        <IcApplyCheckLight />
-      </St.SizeBtnImgBox>
-      <St.SizeBtnTextBox>
-        <St.SizeBtnTitle>{title}</St.SizeBtnTitle>
-        <St.SizeBtnDetail>{detail}</St.SizeBtnDetail>
-      </St.SizeBtnTextBox>
-    </St.SizeBtn>
-  );
-};
+export type Ref = HTMLButtonElement;
+
+const SelectSizeCustomBtn = forwardRef<Ref, SelectSizeCustomBtnProps>(
+  ({ id, title, detail, onClick, selectedBtn }, ref) => {
+    return (
+      <St.SizeBtnWrapper type='button' id={id} onClick={onClick} ref={ref}>
+        {selectedBtn === id ? (
+          <St.SelectedSizeBtnImgBox>
+            <IcApplyCheckPink />
+          </St.SelectedSizeBtnImgBox>
+        ) : (
+          <St.SizeBtnImgBox>
+            <IcApplyCheckLight />
+          </St.SizeBtnImgBox>
+        )}
+        <St.SizeBtnTextBox>
+          <St.SizeBtnTitle>{title}</St.SizeBtnTitle>
+          <St.SizeBtnDetail>{detail}</St.SizeBtnDetail>
+        </St.SizeBtnTextBox>
+      </St.SizeBtnWrapper>
+    );
+  },
+);
 
 export default SelectSizeCustomBtn;
 
+// 버튼 스타일 확장을 위한 styled component 템플릿
+const SizeBtnImgBoxTemplate = styled.div`
+  height: 16rem;
+  width: 100%;
+
+  background: url(${img_home});
+
+  & > svg {
+    padding-top: 0.8rem;
+  }
+
+  pointer-events: none;
+`;
+
 const St = {
-  SizeBtn: styled.button`
+  SizeBtnWrapper: styled.button`
     display: flex;
     flex-direction: column;
 
     width: 16rem;
     height: 20.9rem;
     padding: 0;
-  `,
-  SizeBtnImgBox: styled.div`
-    height: 16rem;
-    width: 100%;
 
-    border-radius: 0.5rem 0.5rem 0 0;
-
-    /* background-color: ${({ theme }) => theme.colors.gray1}; */
-    background: url(${img_home});
-
-    & > svg {
-      padding-top: 0.8rem;
+    &.isSelected {
+      border: 0.1rem solid ${({ theme }) => theme.colors.pink5};
+      border-radius: 0.5rem;
     }
+  `,
+
+  SizeBtnImgBox: styled(SizeBtnImgBoxTemplate)`
+    border-radius: 0.5rem 0.5rem 0 0;
+  `,
+
+  SelectedSizeBtnImgBox: styled(SizeBtnImgBoxTemplate)`
+    border-radius: 0.4rem 0.4rem 0 0;
   `,
 
   SizeBtnTextBox: styled.div`
@@ -59,6 +88,8 @@ const St = {
     border-radius: 0 0 0.5rem 0.5rem;
 
     background-color: ${({ theme }) => theme.colors.bg};
+
+    pointer-events: none;
   `,
 
   SizeBtnTitle: styled.span`
@@ -66,6 +97,8 @@ const St = {
 
     color: ${({ theme }) => theme.colors.gray5};
     ${({ theme }) => theme.fonts.body_medium_16};
+
+    pointer-events: none;
   `,
 
   SizeBtnDetail: styled.span`
@@ -73,5 +106,7 @@ const St = {
 
     color: ${({ theme }) => theme.colors.gray3};
     ${({ theme }) => theme.fonts.detail_medium_12};
+
+    pointer-events: none;
   `,
 };
