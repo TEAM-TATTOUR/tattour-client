9c6522a3a24771a1aaca3bf57f4a5d4f4a70815f
lydiacho <sheecho0804@ewhain.net>
2023-07-09 20:29:15 +0900

    feat: DeliveryInfo 기본 UI 구현

--- src/components/Order/DeliveryInfo.tsx
+++ src/components/Order/DeliveryInfo.tsx
@@ -1,9 +1,53 @@
 import styled from "styled-components"
 
 const DeliveryInfo = () => {
-  return (
-    <div>DeliveryInfo</div>
-  )
+    return (
+        <St.Wrapper>
+            <St.Title>
+                배송 정보 
+            </St.Title>
+            <St.InfoList>
+                <St.Info>
+                    <label for="receiver">수령인</label>
+                    <input type="text" id="receiver" name="receiver" placeholder="홍길동"/>
+                </St.Info>
+                <St.Info>
+                    <label for="phone">연락처</label>
+                    <input type="text" id="phone" name="phone" placeholder="010-0000-0000"/>
+                </St.Info>
+                <St.Info>
+                    <label>배송지</label>
+                    <input type="text" name="posecode" placeholder="우편번호"/>
+                    <input type="text" name="address" placeholder="주소"/>
+                    <input type="text" name="detail" placeholder="상세주소"/>
+                </St.Info>
+            </St.InfoList>
+
+        </St.Wrapper>
+
+    )
 }
 
 export default DeliveryInfo
+
+const St = {
+    Wrapper : styled.section`
+        padding : 2.8rem 0rem 3.6rem 2rem
+    `,
+    Title : styled.h2`
+        margin-left: 0.2rem;
+        ${({ theme }) => theme.fonts.title_semibold_18};
+        color: ${({ theme }) => theme.colors.gray8};
+    `,
+    InfoList : styled.ul`
+        
+
+    `,
+    Info : styled.li`
+    
+    `,
+    SearchBtn : styled.button`
+    
+    `,
+
+}
