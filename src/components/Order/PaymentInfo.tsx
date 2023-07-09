04c2791000bd627c7eba3da8a17eaafa8db9428c
lydiacho <sheecho0804@ewhain.net>
2023-07-09 21:05:14 +0900

    style: 타이틀 UI

--- src/components/Order/PaymentInfo.tsx
+++ src/components/Order/PaymentInfo.tsx
@@ -2,8 +2,24 @@ import styled from "styled-components"
 
 const PaymentInfo = () => {
   return (
-    <div>PaymentInfo</div>
+    <St.Wrapper>
+        <St.Title>
+            결제 정보
+        </St.Title>
+    </St.Wrapper>
   )
 }
 
 export default PaymentInfo
+
+const St = {
+    Wrapper : styled.section`
+        padding : 2.8rem 2rem 3.6rem 2rem
+    `,
+    Title : styled.h2`
+        margin-left: 0.2rem;
+        
+        ${({ theme }) => theme.fonts.title_semibold_18};
+        color: ${({ theme }) => theme.colors.gray8};
+    `,
+}
