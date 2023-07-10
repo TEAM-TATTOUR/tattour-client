4e2b3aa491fc22192f0445e5aa373bc21806c826
seoAreum <1971236@hansung.ac.kr>
2023-07-11 03:15:52 +0900

    feat: 버튼 이벤트 등록

--- src/common/Modal/Modal.tsx
+++ src/common/Modal/Modal.tsx
@@ -1,14 +1,24 @@
 import styled from 'styled-components';
+import { IcCancelDark } from '../../assets/icon';
+import { useNavigate } from 'react-router-dom';
 
 interface ModalProps {
   onClose: () => void;
 }
 
 const Modal: React.FC<ModalProps> = ({ onClose }) => {
+  const navigate = useNavigate();
+
+  const handleClickStopBtn = () => {
+    onClose();
+    navigate('/');
+  };
+
   return (
     <St.ModalContainer>
       <St.ModalContent>
         <St.ModalTitleWrapper>
+          <IcCancelDark onClick={onClose} />
           <St.ModalTitle>정말 그만두시나요?</St.ModalTitle>
           <St.ModalSubTitle>
             프로필 설정만 마치면, 회원가입 과정이 모두 끝납니다. 중간에 나가시면 정보가 사라져요.
@@ -16,8 +26,8 @@ const Modal: React.FC<ModalProps> = ({ onClose }) => {
         </St.ModalTitleWrapper>
 
         <St.BtnWrapper>
-          <St.ContinueBtn>설정 계속하기</St.ContinueBtn>
-          <St.StopBtn onClick={onClose}>그만두기</St.StopBtn>
+          <St.ContinueBtn onClick={onClose}>설정 계속하기</St.ContinueBtn>
+          <St.StopBtn onClick={handleClickStopBtn}>그만두기</St.StopBtn>
         </St.BtnWrapper>
       </St.ModalContent>
     </St.ModalContainer>
@@ -44,7 +54,7 @@ const St = {
     flex-direction: column;
     justify-content: center;
     align-items: center;
-
+    position: relative;
     width: 33.5rem;
     height: 24.1rem;
 
@@ -59,6 +69,12 @@ const St = {
     justify-content: center;
     align-items: center;
     margin-top: 4.7rem;
+
+    & > svg {
+      position: absolute;
+      top: 2.5rem;
+      right: 2rem;
+    }
   `,
 
   ModalTitle: styled.h2`
@@ -68,6 +84,7 @@ const St = {
   `,
 
   ModalSubTitle: styled.p`
+    // width 뷰마다 달라지면 props로 받아와서 적용하도록 수정
     width: 27rem;
     padding-top: 1.6rem;
     padding-bottom: 4rem;
