f7692ac5542305204b49504ccb68188a4f6ab280
Yeonseo-Jo <jys073190@gmail.com>
2023-07-11 02:33:13 +0900

    feat : 미리보기 구현

--- src/components/Custom/NoDesgin/CustomImgAttach.tsx
+++ src/components/Custom/NoDesgin/CustomImgAttach.tsx
@@ -1,13 +1,37 @@
 import { styled } from 'styled-components';
 import { IcPhoto } from '../../../assets/icon';
+import React, { useState } from 'react';
 
 const CustomImgAttach = () => {
+  const [thumbnailURL, setThumbnailURL] = useState<string>('');
+
+  const handleChangeImgAttach = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (!files || !files[0]) return;
+
+    const fileBlob = files[0];
+
+    const reader = new FileReader();
+    reader.readAsDataURL(fileBlob);
+    reader.onloadend = () => {
+      const base64ImgURL = reader.result as string;
+      setThumbnailURL(base64ImgURL);
+      // console.log('fileReader 방식: \n', base64data);
+    };
+  };
+
   return (
     <St.ImgAttachContainer>
-      <input id='img-input' type='file' accept='image/png' />
-      <St.ImgAttachArea>
-        <p>도안 이미지를 첨부해주세요</p>
-      </St.ImgAttachArea>
+      <input id='img-input' type='file' accept='image/png' onChange={handleChangeImgAttach} />
+      {thumbnailURL ? (
+        <St.ImgPreviewArea>
+          <img src={thumbnailURL} alt='그려둔 도안 이미지 미리보기' />
+        </St.ImgPreviewArea>
+      ) : (
+        <St.ImgAttachArea>
+          <p>도안 이미지를 첨부해주세요</p>
+        </St.ImgAttachArea>
+      )}
       <label htmlFor='img-input'>
         <St.ImgAttachBtn>
           <IcPhoto />
@@ -46,6 +70,20 @@ const St = {
     & > p {
       color: ${({ theme }) => theme.colors.gray2};
       ${({ theme }) => theme.fonts.body_medium_14};
+    }
+  `,
+
+  ImgPreviewArea: styled.div`
+    width: 33.5rem;
+    height: 24.6rem;
+
+    background-color: ${({ theme }) => theme.colors.bg};
+
+    & > img {
+      width: 100%;
+      height: 100%;
+
+      object-fit: contain;
     }
   `,
 
