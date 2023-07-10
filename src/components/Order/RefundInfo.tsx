106a6159f64003dbe70818a99dd28af02f9b9841
lydiacho <sheecho0804@ewhain.net>
2023-07-10 23:46:17 +0900

    feat: 체크박스 구현

--- src/components/Order/RefundInfo.tsx
+++ src/components/Order/RefundInfo.tsx
@@ -1,10 +1,13 @@
 import styled from "styled-components"
 import { IcArrowRightDark } from "../../assets/icon"
+import ic_check from '../../assets/icon/ic_check.svg'
+import ic_check_selected from '../../assets/icon/ic_check_selected.svg'
 
 const RefundInfo = () => {
   return (
     <St.Wrapper>
-        <St.Checkbox type="checkbox"/>
+        <St.Checkbox type="checkbox" id="agree"/>
+        <label htmlFor="agree"></label>
         <St.Button>
             <St.Text>타투어 환불 정책에 동의합니다</St.Text>
             <IcArrowRightDark/>
@@ -25,15 +28,23 @@ const St = {
 
     `,
     Checkbox : styled.input`
-        width: 2.4rem;
-        height: 2.4rem;
+        display: none;
 
-        background-color: ${({ theme }) => theme.colors.gray0};
-        border-radius: 0.4rem;
+        & + label {
+            display: block;
 
-        &:checked {
-            background-color: ${({ theme }) => theme.colors.gray6};
-            color: ${({ theme }) => theme.colors.white};
+            width: 2.4rem;
+            height: 2.4rem;
+            
+            background: url(${ic_check});
+        }
+        &:checked + label {
+            display: block;
+
+            width: 2.4rem;
+            height: 2.4rem;
+
+            background: url(${ic_check_selected});
         }
     `,
     Button : styled.article`
