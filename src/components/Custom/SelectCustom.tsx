169fcc5f72caf0871258ab361a182c2b7f3450fe
Yeonseo-Jo <jys073190@gmail.com>
2023-07-10 17:26:45 +0900

    fix : 상황 선택 버튼 컴포넌트로 빼기

--- src/components/Custom/SelectCustom.tsx
+++ src/components/Custom/SelectCustom.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { styled } from 'styled-components';
-import { IcApplyCheckGray, IcApplyCheckPink } from '../../assets/icon';
+import SelectCustomBtn from './SelectCustomBtn';
 
 const SelectCustom = () => {
   const CASE_BTN_DATA = [
@@ -47,17 +47,15 @@ const SelectCustom = () => {
       <St.SelectBtnContainer>
         {CASE_BTN_DATA.map(({ id, title, detail }, idx) => {
           return (
-            <St.SelectBtnContent
-              type='button'
+            <SelectCustomBtn
               key={id}
               id={id}
+              title={title}
+              detail={detail}
               ref={(element: HTMLButtonElement) => (btnRef.current[idx] = element)}
               onClick={handleClickSelBtn}
-            >
-              {activeBtn === id ? <IcApplyCheckPink /> : <IcApplyCheckGray />}
-              <St.SelectBtnTitle $case={id}>{title}</St.SelectBtnTitle>
-              <St.SelectBtnDetail $case={id}>{detail}</St.SelectBtnDetail>
-            </St.SelectBtnContent>
+              isSelected={activeBtn === id}
+            />
           );
         })}
       </St.SelectBtnContainer>
@@ -99,50 +97,5 @@ const St = {
   SelectBtnContainer: styled.article`
     display: flex;
     gap: 1.5rem;
-  `,
-
-  SelectBtnContent: styled.button`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 0.8rem;
-
-    width: 16rem;
-    height: 16.8rem;
-    padding: 0.8rem 0 3rem;
-
-    border-radius: 0.5rem;
-    background-color: ${({ theme }) => theme.colors.bg};
-
-    &.isClicked {
-      border: 0.1rem solid ${({ theme }) => theme.colors.pink5};
-    }
-  `,
-
-  SelectBtnTitle: styled.h3<{ $case: string }>`
-    width: ${({ $case }) => ($case === 'haveDesign' ? 10 : 8.6)}rem;
-
-    color: ${({ theme }) => theme.colors.gray7};
-    ${({ theme }) => theme.fonts.title_semibold_16};
-
-    text-align: center;
-
-    white-space: pre-line;
-
-    pointer-events: none;
-  `,
-
-  SelectBtnDetail: styled.p<{ $case: string }>`
-    width: ${({ $case }) => ($case === 'haveDesign' ? 11.6 : 10.6)}rem;
-
-    color: ${({ theme }) => theme.colors.gray3};
-    ${({ theme }) => theme.fonts.detail_medium_12};
-
-    text-align: center;
-
-    white-space: pre-line;
-
-    pointer-events: none;
   `,
 };
