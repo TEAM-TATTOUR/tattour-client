70398fa2da53f572c980dbf47ac6e6d89040eff0
Yeonseo-Jo <jys073190@gmail.com>
2023-07-10 22:27:36 +0900

    feat : 커스텀 이미지 페이지 라우팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import MainPage from './pages/MainPage';
 import LoginPage from './pages/Login/LoginPage';
 import RegisterPage from './pages/Register/RegisterPage';
 import RegisterPhoneNum from './components/Register/RegisterPhoneNum';
+import CustomImgPage from './pages/Custom/NoDesign/CustomImgPage';
 
 const Router = () => {
   return (
@@ -12,6 +13,7 @@ const Router = () => {
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
         <Route path='/input-number' element={<RegisterPhoneNum />} />
+        <Route path='/custom-img' element={<CustomImgPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Custom/NoDesign/CustomImgPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CustomImgPage = () => {
+  return <div>이미지</div>;
+};
+
+export default CustomImgPage;
