550770918e99eb82ea38fbb33c5da61ea8af4308
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-07-10 23:12:42 +0900

    feat: 첨부하기 페이지 푸터 따로 뺌

--- src/pages/CustomReference/CustomReferencePage.tsx
+++ src/pages/CustomReference/CustomReferencePage.tsx
@@ -1,6 +1,7 @@
 import { IcBackLight, IcCloseLight } from '../../assets/icon/index';
 import CustomFooter from '../../components/Custom/Common/CustomFooter';
 import CustomReference from '../../components/CustomReference/CustomReference';
+import CustomReferenceFooter from '../../components/CustomReference/CustomReferenceFooter';
 import Header from '../../components/Header';
 import PageLayout from '../../components/PageLayout';
 
@@ -12,8 +13,9 @@ const CustomReferencePage = () => {
   };
 
   return (
-    <PageLayout renderHeader={renderCustomReferencePageHeader} footer={<CustomFooter />}>
+    <PageLayout renderHeader={renderCustomReferencePageHeader}>
       <CustomReference />
+      <CustomReferenceFooter />
     </PageLayout>
   );
 };
