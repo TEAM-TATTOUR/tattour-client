62ad144d6aa528f20fa03d1b87eb8bd160e84636
seoAreum <1971236@hansung.ac.kr>
2023-07-11 02:48:47 +0900

    feat: 모달 스타일 지정

--- src/common/Modal/Modal.tsx
+++ src/common/Modal/Modal.tsx
@@ -1,50 +1,116 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 interface ModalProps {
-    onClose: () => void;
-  }
-  
-  const Modal: React.FC<ModalProps> = ({ onClose }) => {
-    return (
-      <ModalContainer>
-        <ModalContent>
-          {/* <ModalTitle>정말 그만두시나요?</.ModalTitle> */}
-          <GoBackBtn onClick={onClose}>게임으로 돌아가기</GoBackBtn>
-        </ModalContent>
-      <ModalContainer>
-    );
-  };
+  onClose: () => void;
+}
 
+const Modal: React.FC<ModalProps> = ({ onClose }) => {
+  return (
+    <St.ModalContainer>
+      <St.ModalContent>
+        <St.ModalTitleWrapper>
+          <St.ModalTitle>정말 그만두시나요?</St.ModalTitle>
+          <St.ModalSubTitle>
+            프로필 설정만 마치면, 회원가입 과정이 모두 끝납니다. 중간에 나가시면 정보가 사라져요.
+          </St.ModalSubTitle>
+        </St.ModalTitleWrapper>
 
-const ModalContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+        <St.BtnWrapper>
+          <St.ContinueBtn>설정 계속하기</St.ContinueBtn>
+          <St.StopBtn onClick={onClose}>그만두기</St.StopBtn>
+        </St.BtnWrapper>
+      </St.ModalContent>
+    </St.ModalContainer>
+  );
+};
 
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
+const St = {
+  ModalContainer: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-  background: rgba(0, 0, 0, 0.25);
-`;
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100vh;
 
-const ModalContent = styled.div`
-  display: grid;
-  padding: 2.5rem 2.5rem;
-  border-radius: 1.5rem;
-  background-color: ${({ theme }) => theme.colors.gray2};
-  font-size: 1.7rem;
-`;
+    background: rgba(0, 0, 0, 0.25);
+  `,
 
-const GoBackBtn = styled.button`
-  padding: 1rem 0rem;
-  margin-top: 1.5rem;
-  border: 0;
-  border-radius: 1rem;
-  background-color: ${({ theme }) => theme.colors.gray5};
-  color: ${({ theme }) => theme.colors.white};
-`;
+  ModalContent: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 33.5rem;
+    height: 24.1rem;
+
+    border-radius: 1rem;
+
+    background-color: ${({ theme }) => theme.colors.white};
+  `,
+
+  ModalTitleWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 4.7rem;
+  `,
+
+  ModalTitle: styled.h2`
+    color: ${({ theme }) => theme.colors.gray7};
+
+    ${({ theme }) => theme.fonts.title_semibold_20};
+  `,
+
+  ModalSubTitle: styled.p`
+    width: 27rem;
+    padding-top: 1.6rem;
+    padding-bottom: 4rem;
+
+    color: ${({ theme }) => theme.colors.gray3};
+
+    ${({ theme }) => theme.fonts.body_medium_16};
+  `,
+
+  BtnWrapper: styled.div`
+    display: flex;
+    align-items: flex-end;
+    width: 100%;
+    padding: 0;
+  `,
+
+  ContinueBtn: styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 16.7rem;
+    height: 7rem;
+
+    border-bottom-left-radius: 1rem;
+    background-color: ${({ theme }) => theme.colors.gray8};
+    color: ${({ theme }) => theme.colors.white};
+
+    ${({ theme }) => theme.fonts.title_semibold_18};
+  `,
+
+  StopBtn: styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 16.8rem;
+    height: 7rem;
+
+    border-bottom-right-radius: 1rem;
+    background-color: ${({ theme }) => theme.colors.gray3};
+    color: ${({ theme }) => theme.colors.white};
+
+    ${({ theme }) => theme.fonts.title_semibold_18};
+  `,
+};
 
 export default Modal;
