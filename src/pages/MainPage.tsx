ecc6659fb152be8f956b654746da387d4de04c4b
seoAreum <1971236@hansung.ac.kr>
2023-07-12 01:51:24 +0900

    feat: progress bar 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,15 +1,30 @@
+import { useState } from 'react';
 import MainFooter from '../components/MainFooter';
 import Header from '../components/Header';
 import PageLayout from '../components/PageLayout';
+import ProgressBar from '../common/ProgressBar';
+import BackBtn from '../utils/BackBtn';
+import CancelBtn from '../utils/CancelBtn';
+import EscapeModal from '../common/Modal/EscapeModal/EscapeModal'
 
 const MainPage = () => {
+  const [modalOn, setModalOn] = useState(false);
+
   const reanderMainPageHeader = () => {
     return (
       <Header
         transparent={true}
-        leftSection={<div>left</div>}
+        backBtn={<BackBtn />}
         title='Test'
-        rightSection={<div>right</div>}
+        cancelBtn={
+          <CancelBtn
+            modalOn={modalOn}
+            setModalOn={setModalOn}
+            children={<EscapeModal setModalOn={setModalOn} />}
+          />
+        }
+        // 전체 페이지수, 현재 페이지 단계 알아서 넣기
+        progressBar={<ProgressBar curStep={1} maxStep={3} />}
       />
     );
   };
