4cc6292cdb922d3d474a88567a0613e79f99d22b
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-07-11 16:43:00 +0900

    chore: 파일구조 변경

---
+++ src/components/Custom/Design/CustomReference.tsx
@@ -0,0 +1,24 @@
+import { styled } from 'styled-components';
+import CustomTitle from './CustomTitle';
+import CustomImageAttach from './CustomImageAttach';
+
+const CustomReference = () => {
+  return (
+    <St.PageWrapper>
+      <CustomTitle />
+      <CustomImageAttach />
+    </St.PageWrapper>
+  );
+};
+
+const St = {
+  PageWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    padding: 0 2rem;
+  `,
+};
+
+export default CustomReference;
