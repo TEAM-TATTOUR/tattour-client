f762d9c389ce22ef045b0e595fde37baa3292250
seoAreum <1971236@hansung.ac.kr>
2023-07-11 21:57:41 +0900

    feat: input 입력 타입 제한

--- src/utils/sliceMaxLength.ts
+++ src/utils/sliceMaxLength.ts
@@ -1,6 +1,23 @@
 import React from 'react';
 
-const sliceMaxLength = (e: React.ChangeEvent<HTMLInputElement>, maxLength: number) => {
+const sliceMaxLength = (
+  e: React.ChangeEvent<HTMLInputElement>,
+  maxLength: number,
+  inputType: string,
+) => {
+  switch (inputType) {
+    case 'onlyNum':
+      e.target.value = e.target.value.replace(/[^0-9]/g, '');
+      break;
+
+    case 'phoneNum':
+      e.target.value = e.target.value.replace(/[^0123456789-]/g, '');
+      break;
+
+    case 'onlyString':
+      e.target.value = e.target.value.replace(/[^ㄱ-힣a-zA-Z]/g, '');
+  }
+
   if (e.target.value.length > maxLength) {
     e.target.value = e.target.value.slice(0, maxLength);
   }
