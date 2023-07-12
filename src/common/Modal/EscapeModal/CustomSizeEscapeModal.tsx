9c5a265d512ed19bd30ef94fbc37ec4e998a36b9
seoAreum <1971236@hansung.ac.kr>
2023-07-13 04:27:32 +0900

    style: ux라이팅 반영

--- src/common/Modal/EscapeModal/CustomSizeEscapeModal.tsx
+++ src/common/Modal/EscapeModal/CustomSizeEscapeModal.tsx
@@ -13,7 +13,7 @@ const CustomSizeEscapeModal = ({setModalOn} : CustomSizeEscapeModalProps) => {
         pageName={'CustomSizePage'}
         title={'정말 그만두시나요?'}
         subTitle={'신청서는 자동으로 임시저장되어 언제든 이어서 작성할 수 있어요'}
-        continueBtn={'임시저장'}
+        continueBtn={'취소'}
         stopBtn={'그만하기'}
       />
     </ModalPortal>
