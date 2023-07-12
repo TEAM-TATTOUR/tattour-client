2608580fcd54e8d069b643ac4a5b3ba5f293725e
lydiacho <sheecho0804@ewhain.net>
2023-07-12 21:18:29 +0900

    feat: BottomSheet 재활용

--- src/components/Order/RefundBottom.tsx
+++ src/components/Order/RefundBottom.tsx
@@ -1,9 +1,72 @@
 import styled from "styled-components"
+import Sheet from 'react-modal-sheet';
+
 
 const RefundBottom = () => {
   return (
-    <div>RefundBottom</div>
+    <CustomSheet 
+        key={filter.type}
+        isOpen={filter.isOpen} 
+        onClose={filter.onClose}
+        detent="content-height"
+        disableDrag={true}
+        >
+        <Sheet.Container>
+            <Sheet.Header disableDrag={true}/>
+            <Sheet.Content>
+                {filter.data.map((el)=>(
+                    <St.TagBox key={el}>{el}</St.TagBox>
+                ))}
+                <St.Footer>
+                    <St.Button type='button'>
+                        적용하기
+                    </St.Button>
+                </St.Footer>
+            </Sheet.Content>
+        </Sheet.Container>
+        <Sheet.Backdrop onClick={filter.onClose}/>
+    </CustomSheet>
   )
 }
 
 export default RefundBottom
+
+const St = {
+    TagBox : styled.p`
+        text-align: center;
+        width:100%;
+        padding: 1.7rem 0rem;
+        color: ${({ theme }) => theme.colors.gray4};
+        ${({ theme }) => theme.fonts.title_medium_18};
+    `,
+    Footer: styled.footer`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 7rem;
+        margin-top: 4rem;
+        background-color: ${({ theme }) => theme.colors.gray3};
+    `,
+    Button: styled.button`
+        width: 100%;
+        height: 100%;
+        color: ${({ theme }) => theme.colors.white};
+        font: ${({ theme }) => theme.fonts.title_semibold_18};
+    `,
+}
+
+const CustomSheet = styled(Sheet)`
+    .react-modal-sheet-backdrop {
+        background-color: rgba(0, 0, 0, 0.6) !important;
+    }
+    .react-modal-sheet-container {
+        border-radius: 1rem !important;
+    }
+    .react-modal-sheet-header {
+        height: 1.6rem !important;
+    }
+    .react-modal-sheet-drag-indicator {
+        display: none;
+    }
+`
