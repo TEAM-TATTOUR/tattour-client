733d97706491a4bfaf52e77517c3d97e221799f0
Yeonseo-Jo <jys073190@gmail.com>
2023-07-12 17:47:41 +0900

    feat : 기본 UI 구현

--- src/components/PointCharge/Charge.tsx
+++ src/components/PointCharge/Charge.tsx
@@ -1,5 +1,67 @@
+import { styled } from 'styled-components';
+
 const Charge = () => {
-  return <div>충전페이지 input 넣을 컴포넌트</div>;
+  return (
+    <St.ChargeWrapper>
+      <St.ChargeInfoContainer>
+        <St.ChargeInfoTitle>충전할 금액을 알려주세요</St.ChargeInfoTitle>
+        <St.ChargeInfoDetail>
+          상품 구매, 신청서 작성 등 타투어 내 거래에 필요한 머니에요.
+        </St.ChargeInfoDetail>
+      </St.ChargeInfoContainer>
+      <St.ChargeInputContainer>
+        <St.ChargeInput type='text' placeholder='1,000원 단위로 입력해주세요' autoFocus />
+      </St.ChargeInputContainer>
+    </St.ChargeWrapper>
+  );
 };
 
 export default Charge;
+
+const St = {
+  ChargeWrapper: styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3rem; /*디테일 설명과 input 사이 간격 */
+
+    width: 100%;
+  `,
+
+  ChargeInfoContainer: styled.article`
+    display: flex;
+    flex-direction: column;
+    gap: 1.2rem;
+
+    margin: 5.6rem 2.9rem 0 2.2rem;
+  `,
+
+  ChargeInfoTitle: styled.h2`
+    color: ${({ theme }) => theme.colors.gray8};
+    ${({ theme }) => theme.fonts.title_semibold_20};
+  `,
+
+  ChargeInfoDetail: styled.p`
+    color: ${({ theme }) => theme.colors.gray3};
+    ${({ theme }) => theme.fonts.body_medium_14};
+  `,
+
+  ChargeInputContainer: styled.article``,
+
+  ChargeInput: styled.input`
+    width: 33.5rem;
+    height: 4.5rem;
+    padding: 1.2rem 2rem;
+
+    ${({ theme }) => theme.fonts.body_medium_16};
+
+    color: ${({ theme }) => theme.colors.gray5};
+    background-color: ${({ theme }) => theme.colors.bg};
+
+    box-sizing: border-box;
+    border-radius: 0.5rem;
+    border: 0;
+
+    text-align: right;
+  `,
+};
