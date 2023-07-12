e07fb7034da8174297f86a5adad0d3af4c46faed
seoAreum <1971236@hansung.ac.kr>
2023-07-13 05:04:17 +0900

    feat: 송금 확인 뷰 구현

---
+++ src/common/Modal/CheckModal/CheckModalForm.tsx
@@ -0,0 +1,118 @@
+import styled from 'styled-components';
+import { IcCancelDark } from '../../../assets/icon';
+import { useNavigate } from 'react-router-dom';
+
+interface CheckModalFormProps {
+  onClose: () => void;
+  title: string;
+  subTitle: string;
+  continueBtn: string;
+}
+
+const CheckModalForm = ({
+  onClose,
+  title,
+  subTitle,
+  continueBtn,
+}: CheckModalFormProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <St.ModalContainer>
+      <St.ModalContent>
+        <St.ModalTitleWrapper>
+          <IcCancelDark onClick={onClose} />
+          <St.ModalTitle>{title}</St.ModalTitle>
+          <St.ModalSubTitle>{subTitle}</St.ModalSubTitle>
+        </St.ModalTitleWrapper>
+
+        <St.BtnWrapper>
+          <St.ContinueBtn onClick={onClose}>{continueBtn}</St.ContinueBtn>
+        </St.BtnWrapper>
+      </St.ModalContent>
+    </St.ModalContainer>
+  );
+};
+
+const St = {
+  ModalContainer: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100vh;
+
+    background: rgba(0, 0, 0, 0.6);
+  `,
+
+  ModalContent: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    width: 33.5rem;
+    height: 24.1rem;
+
+    border-radius: 1rem;
+
+    background-color: ${({ theme }) => theme.colors.white};
+  `,
+
+  ModalTitleWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 4.7rem;
+
+    & > svg {
+      position: absolute;
+      top: 2.5rem;
+      right: 2rem;
+    }
+  `,
+
+  ModalTitle: styled.h2`
+    color: ${({ theme }) => theme.colors.gray7};
+
+    ${({ theme }) => theme.fonts.title_semibold_20};
+  `,
+
+  ModalSubTitle: styled.p`
+    padding: 1.6rem 7.25rem 4rem 7.25rem;
+
+    text-align: center;
+    color: ${({ theme }) => theme.colors.gray3};
+
+    ${({ theme }) => theme.fonts.body_medium_16};
+  `,
+
+  BtnWrapper: styled.div`
+    display: flex;
+    align-items: flex-end;
+    width: 100%;
+    padding: 0;
+  `,
+
+  ContinueBtn: styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 7rem;
+
+    border-bottom-left-radius: 1rem;
+    border-bottom-right-radius: 1rem;
+    background-color: ${({ theme }) => theme.colors.gray8};
+    color: ${({ theme }) => theme.colors.white};
+
+    ${({ theme }) => theme.fonts.title_semibold_18};
+  `,
+};
+
+export default CheckModalForm;
