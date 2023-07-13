f5b98bebcd253e288da88cc61c7b27778ccec3ae
seoAreum <1971236@hansung.ac.kr>
2023-07-13 23:02:40 +0900

    refactor: 타입 변화로 코드 수정

--- src/pages/Error/ErrorPage.tsx
+++ src/pages/Error/ErrorPage.tsx
@@ -2,7 +2,7 @@ import PreventAccess from '../../common/PreventAccess/PreventAccess';
 import { TITLE, BTN_CONTENTS } from '../../constants/PreventAccessInfo';
 
 const ErrorPage = () => {
-  return <PreventAccess title={TITLE[0]} btn_contents={BTN_CONTENTS[0]} isError={true}/>;
+  return <PreventAccess title={TITLE.error} btn_contents={BTN_CONTENTS.error} isError={true}/>;
 };
 
 export default ErrorPage;
--- src/pages/Expiration/ExpirationPage.tsx
+++ src/pages/Expiration/ExpirationPage.tsx
@@ -2,7 +2,7 @@ import PreventAccess from '../../common/PreventAccess/PreventAccess';
 import { BTN_CONTENTS, TITLE } from '../../constants/PreventAccessInfo';
 
 const ExpirationPage = () => {
-  return <PreventAccess title={TITLE[1]} btn_contents={BTN_CONTENTS[1]} />;
+  return <PreventAccess title={TITLE.expiration} btn_contents={BTN_CONTENTS.expiration} />;
 };
 
 export default ExpirationPage;
