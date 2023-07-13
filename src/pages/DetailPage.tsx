b11887227246846629e095a6ca412a88d65a7751
lydiacho <sheecho0804@ewhain.net>
2023-07-14 01:23:20 +0900

    fix: <div> -> <>

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,14 +1,14 @@
-import styled from "styled-components"
-import DetailInfo from "../components/Detail/DetailInfo"
-import DetailCarousel from "../components/Detail/DetailCarousel"
+import styled from 'styled-components';
+import DetailInfo from '../components/Detail/DetailInfo';
+import DetailCarousel from '../components/Detail/DetailCarousel';
 
 const DetailPage = () => {
   return (
-    <div>
-        <DetailCarousel/>
-        <DetailInfo/>
-    </div>
-  )
-}
+    <>
+      <DetailCarousel />
+      <DetailInfo />
+    </>
+  );
+};
 
-export default DetailPage
+export default DetailPage;
