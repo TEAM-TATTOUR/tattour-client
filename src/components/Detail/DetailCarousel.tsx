a63f20f0d07fb4cd3593739fdc86f7a8803b6ccb
lydiacho <sheecho0804@ewhain.net>
2023-07-13 16:58:00 +0900

    feat: Carousel 모습 구현

--- src/components/Detail/DetailCarousel.tsx
+++ src/components/Detail/DetailCarousel.tsx
@@ -8,13 +8,7 @@ import { Pagination } from 'swiper/modules';
 const DetailCarousel = () => {
   return (
     <St.Wrapper>
-      <Swiper
-        modules={[Pagination]}
-        pagination={{ clickable: true }}
-        speed={1000}
-        slidesPerView={1}
-        loop={true}
-      >
+      <Swiper modules={[Pagination]} pagination={{ clickable: true }} slidesPerView={1} loop={true}>
         <SwiperSlide>
           <St.Card>Slide 1</St.Card>
         </SwiperSlide>
@@ -23,9 +17,6 @@ const DetailCarousel = () => {
         </SwiperSlide>
         <SwiperSlide>
           <St.Card>Slide 3</St.Card>
-        </SwiperSlide>
-        <SwiperSlide>
-          <St.Card>Slide 4</St.Card>
         </SwiperSlide>
       </Swiper>
     </St.Wrapper>
@@ -36,26 +27,35 @@ export default DetailCarousel;
 
 const St = {
   Wrapper: styled.section`
-    margin-top: 3rem;
-    padding: 0rem 15rem;
+    & .swiper-slide {
+      background-color: ${({ theme }) => theme.colors.gray0};
+    }
 
     & .swiper-pagination {
-      position: relative;
-      margin-top: 1rem;
+      margin-bottom: 2rem;
+    }
+
+    & .swiper-pagination-bullet {
+      width: 0.6rem;
+      height: 0.6rem;
+
+      background-color: ${({ theme }) => theme.colors.white};
+      opacity: 1 !important;
     }
 
     & .swiper-pagination-bullet.swiper-pagination-bullet-active {
-      background-color: pink;
+      background-color: ${({ theme }) => theme.colors.gray8};
     }
 
-    & .swpier-wrapper {
+    & .swiper-wrapper {
       transition-timing-function: linear;
     }
   `,
   Card: styled.div`
-    height: 10rem;
-    background-color: pink;
+    width: 100%;
+    height: 23.5rem;
+
+    background-color: ${({ theme }) => theme.colors.gray0};
     color: black;
-    border: 0.5rem solid rgb(255, 134, 154);
   `,
 };
