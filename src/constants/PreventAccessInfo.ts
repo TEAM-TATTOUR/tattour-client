641a95c6d020e94d2a7a064e54207134437857fd
seoAreum <1971236@hansung.ac.kr>
2023-07-13 23:01:48 +0900

    refactor: 배열에서 객체로 수정

--- src/constants/PreventAccessInfo.ts
+++ src/constants/PreventAccessInfo.ts
@@ -1,2 +1,8 @@
-export const TITLE = ['정보를 불러올 수 없어요', '세션이 만료되었어요'];
-export const BTN_CONTENTS = ['다시 불러오기', '홈으로 돌아가기']
+export const TITLE = {
+  error: '정보를 불러올 수 없어요',
+  expiration: '세션이 만료되었어요',
+};
+export const BTN_CONTENTS = {
+  error: '다시 불러오기',
+  expiration: '홈으로 돌아가기',
+};
