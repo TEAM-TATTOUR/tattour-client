3764c6fa70a543edb129a667edd08f0e33557d49
lydiacho <sheecho0804@ewhain.net>
2023-07-13 16:20:05 +0900

    feat: Carousel 세팅 중

--- src/components/Detail/DetailCarousel.tsx
+++ src/components/Detail/DetailCarousel.tsx
@@ -1,9 +1,32 @@
+import "swiper/css";
+import "swiper/css/pagination";
 import styled from "styled-components"
 
+import { useEffect, useState } from "react";
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+
 const DetailCarousel = () => {
-  return (
-    <div>DetailCarousel</div>
-  )
+    return (
+        <div className="swiper_container">
+            <Swiper
+            modules={[Pagination]}
+            pagination={{ clickable: true }}
+            autoplay={{
+                delay: 2000,
+            }}
+            speed={1000}
+            slidesPerView={1}
+            loop={true}
+            >
+                <SwiperSlide><div className="one_card">Slide 1</div></SwiperSlide>
+                <SwiperSlide><div className="one_card">Slide 2</div></SwiperSlide>
+                <SwiperSlide><div className="one_card">Slide 3</div></SwiperSlide>
+                <SwiperSlide><div className="one_card">Slide 4</div></SwiperSlide>
+            </Swiper>
+        </div>
+    )
 }
 
 export default DetailCarousel
