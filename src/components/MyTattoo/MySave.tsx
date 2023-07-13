36625c0d1b9ca79cd3e2d13649a41f331cc9f4c6
gunom <gu3062@naver.com>
2023-07-14 02:00:59 +0900

    feat: 이미지 저장 없을 시 보이는 뷰 반영

--- src/components/MyTattoo/MySave.tsx
+++ src/components/MyTattoo/MySave.tsx
@@ -5,22 +5,25 @@ const dummyMySaveData = [
   {
     id: 1,
     title: '타투 제목',
+    haveReferenceImage: true,
   },
   {
     id: 2,
     title: '타투 제목',
+    haveReferenceImage: false,
   },
   {
     id: 3,
     title: '타투 제목',
+    haveReferenceImage: true,
   },
 ];
 
 const MySave = () => {
   return (
     <BigScrollContainer title={'SAVE'}>
-      {dummyMySaveData.map(({ id, title }) => {
-        return <MySaveItem key={id} title={title} />;
+      {dummyMySaveData.map(({ id, title, haveReferenceImage }) => {
+        return <MySaveItem key={id} title={title} haveReferenceImage={haveReferenceImage} />;
       })}
     </BigScrollContainer>
   );
