c8cd2d08d03984c7d02d043ecb3b2442940d1323
Yeonseo-Jo <jys073190@gmail.com>
2023-07-14 03:28:18 +0900

    feat : 송금 페이지 헤더 레이아웃

--- src/pages/pointCharge/TransferPage.tsx
+++ src/pages/pointCharge/TransferPage.tsx
@@ -1,12 +1,28 @@
+import BackBtn from '../../common/Header/BackBtn';
+import CancelBtn from '../../common/Header/CancelBtn';
+import ProgressBar from '../../common/ProgressBar';
+import Header from '../../components/Header';
+import PageLayout from '../../components/PageLayout';
 import TransferMain from '../../components/PointCharge/TransferMain';
 import TransferPolicy from '../../components/PointCharge/TransferPolicy';
 
 const TransferPage = () => {
+  const renderTransferPageHeader = () => {
+    return (
+      <Header
+        leftSection={<BackBtn />}
+        title='포인트 충전'
+        rightSection={<CancelBtn />}
+        progressBar={<ProgressBar curStep={2} maxStep={2} />}
+      />
+    );
+  };
+
   return (
-    <>
+    <PageLayout renderHeader={renderTransferPageHeader} footer={<></>}>
       <TransferMain />
       <TransferPolicy />
-    </>
+    </PageLayout>
   );
 };
 
