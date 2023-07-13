eb63c5782d1cb2b0e51ecbc0cb32d667cecd6e64
lydiacho <sheecho0804@ewhain.net>
2023-07-13 16:40:35 +0900

    feat: Carousel 추가

--- src/components/Detail/DetailCarousel.tsx
+++ src/components/Detail/DetailCarousel.tsx
@@ -1,33 +1,64 @@
-import "swiper/css";
-import "swiper/css/pagination";
-import styled from "styled-components"
+import 'swiper/css';
+import 'swiper/css/pagination';
+import styled from 'styled-components';
 
-import { useEffect, useState } from "react";
-import { Pagination } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
 
 const DetailCarousel = () => {
-    return (
-        <div className="swiper_container">
-            <Swiper
-            modules={[Pagination]}
-            pagination={{ clickable: true }}
-            autoplay={{
-                delay: 2000,
-            }}
-            speed={1000}
-            slidesPerView={1}
-            loop={true}
-            >
-                <SwiperSlide><div className="one_card">Slide 1</div></SwiperSlide>
-                <SwiperSlide><div className="one_card">Slide 2</div></SwiperSlide>
-                <SwiperSlide><div className="one_card">Slide 3</div></SwiperSlide>
-                <SwiperSlide><div className="one_card">Slide 4</div></SwiperSlide>
-            </Swiper>
-        </div>
-    )
-}
+  return (
+    <St.Wrapper>
+      <Swiper
+        modules={[Pagination]}
+        pagination={{ clickable: true }}
+        autoplay={{
+          delay: 2000,
+        }}
+        speed={1000}
+        slidesPerView={1}
+        loop={true}
+      >
+        <SwiperSlide>
+          <St.Card>Slide 1</St.Card>
+        </SwiperSlide>
+        <SwiperSlide>
+          <St.Card>Slide 2</St.Card>
+        </SwiperSlide>
+        <SwiperSlide>
+          <St.Card>Slide 3</St.Card>
+        </SwiperSlide>
+        <SwiperSlide>
+          <St.Card>Slide 4</St.Card>
+        </SwiperSlide>
+      </Swiper>
+    </St.Wrapper>
+  );
+};
 
-export default DetailCarousel
+export default DetailCarousel;
 
+const St = {
+  Wrapper: styled.section`
+    margin-top: 3rem;
+    padding: 0rem 15rem;
+
+    & .swiper-pagination {
+      position: relative;
+      margin-top: 1rem;
+    }
+
+    & .swiper-pagination-bullet.swiper-pagination-bullet-active {
+      background-color: pink;
+    }
+
+    & .swpier-wrapper {
+      transition-timing-function: linear;
+    }
+  `,
+  Card: styled.div`
+    height: 10rem;
+    background-color: pink;
+    color: black;
+    border: 0.5rem solid rgb(255, 134, 154);
+  `,
+};
