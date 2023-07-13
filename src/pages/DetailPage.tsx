c706895da3018b824417eab92bcc41b6c25fe557
lydiacho <sheecho0804@ewhain.net>
2023-07-13 21:27:01 +0900

    feat: Bottom 옮겨붙이기

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,12 +1,17 @@
-import styled from "styled-components"
-import DetailInfo from "../components/Detail/DetailInfo"
+import styled from 'styled-components';
+import DetailInfo from '../components/Detail/DetailInfo';
+import DetailBottom from '../components/Detail/DetailBottom';
+import { useState } from 'react';
 
 const DetailPage = () => {
+  const [isSheetOpen, setSheetOpen] = useState(true);
+
   return (
     <div>
-        <DetailInfo/>
+      <DetailInfo setSheetOpen={setSheetOpen} />
+      <DetailBottom isSheetOpen={isSheetOpen} setSheetOpen={setSheetOpen} />
     </div>
-  )
-}
+  );
+};
 
-export default DetailPage
+export default DetailPage;
