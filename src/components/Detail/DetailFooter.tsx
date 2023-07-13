0e092002bfe99f86e99a7ff5bb973ca33dd4bfc2
lydiacho <sheecho0804@ewhain.net>
2023-07-13 18:12:26 +0900

    style: Footer 위치 수정

--- src/components/Detail/DetailFooter.tsx
+++ src/components/Detail/DetailFooter.tsx
@@ -31,6 +31,10 @@ const St = {
     display: flex;
     justify-content: center;
     align-items: center;
+
+    position: fixed;
+    bottom: 0;
+
     width: 100%;
     height: 7rem;
     background-color: ${({ theme }) => theme.colors.gray9};
