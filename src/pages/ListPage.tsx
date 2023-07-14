e1989cf9b5d5d2a07ee651ee6e26b18ff81bbc04
lydiacho <sheecho0804@ewhain.net>
2023-07-14 18:39:58 +0900

    chore: 프리티어 적용

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,48 +1,57 @@
-import FilterBottom from "../components/List/FilterBottom";
-import TattooList from "../components/List/TattooList";
-import styled from "styled-components";
+import FilterBottom from '../components/List/FilterBottom';
+import TattooList from '../components/List/TattooList';
+import styled from 'styled-components';
 import { useState } from 'react';
 
 const ListPage = () => {
   const [isSortOpen, setSortOpen] = useState(false);
   const [isGenreOpen, setGenreOpen] = useState(false);
   const [isStyleOpen, setStyleOpen] = useState(false);
-  const [buttonName, setButtonName] = useState(['정렬','장르','스타일']);
+  const [buttonName, setButtonName] = useState(['정렬', '장르', '스타일']);
   const [selectedIdx, setSelectedIdx] = useState(99); // 선택한 태그의 인덱스 저장
   const [isSelected, setSelected] = useState(false);
   const [selectedFilterIdx, setSelectedFilterIdx] = useState(4);
 
-
   return (
     <div>
-        ListPage
-        <St.Line/>
-        <TattooList
-          isSortOpen={isSortOpen} setSortOpen={setSortOpen}
-          isGenreOpen={isGenreOpen} setGenreOpen={setGenreOpen}
-          isStyleOpen={isStyleOpen} setStyleOpen={setStyleOpen}
-          buttonName={buttonName}
-        />
-        <FilterBottom
-          isSortOpen={isSortOpen} setSortOpen={setSortOpen}
-          isGenreOpen={isGenreOpen} setGenreOpen={setGenreOpen}
-          isStyleOpen={isStyleOpen} setStyleOpen={setStyleOpen}
-          buttonName={buttonName} setButtonName={setButtonName}
-          selectedIdx={selectedIdx} setSelectedIdx={setSelectedIdx}
-          isSelected={isSelected} setSelected={setSelected}
-          selectedFilterIdx={selectedFilterIdx} setSelectedFilterIdx={setSelectedFilterIdx}
-        />
+      ListPage
+      <St.Line />
+      <TattooList
+        isSortOpen={isSortOpen}
+        setSortOpen={setSortOpen}
+        isGenreOpen={isGenreOpen}
+        setGenreOpen={setGenreOpen}
+        isStyleOpen={isStyleOpen}
+        setStyleOpen={setStyleOpen}
+        buttonName={buttonName}
+      />
+      <FilterBottom
+        isSortOpen={isSortOpen}
+        setSortOpen={setSortOpen}
+        isGenreOpen={isGenreOpen}
+        setGenreOpen={setGenreOpen}
+        isStyleOpen={isStyleOpen}
+        setStyleOpen={setStyleOpen}
+        buttonName={buttonName}
+        setButtonName={setButtonName}
+        selectedIdx={selectedIdx}
+        setSelectedIdx={setSelectedIdx}
+        isSelected={isSelected}
+        setSelected={setSelected}
+        selectedFilterIdx={selectedFilterIdx}
+        setSelectedFilterIdx={setSelectedFilterIdx}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default ListPage
+export default ListPage;
 
 const St = {
-    Line : styled.hr`
-        height: 1.3rem;
+  Line: styled.hr`
+    height: 1.3rem;
 
-        background-color: ${({ theme }) => theme.colors.bg};
-        border-width: 0rem;
-    `
-}
+    background-color: ${({ theme }) => theme.colors.bg};
+    border-width: 0rem;
+  `,
+};
