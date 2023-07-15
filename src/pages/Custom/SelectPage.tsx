61f965e543e9397f84d3478de3d1e351e229feb9
Yeonseo-Jo <jys073190@gmail.com>
2023-07-16 01:08:16 +0900

    feat : 헤더 푸터 레이아웃 구현

--- src/pages/Custom/SelectPage.tsx
+++ src/pages/Custom/SelectPage.tsx
@@ -1,11 +1,43 @@
+import { styled } from 'styled-components';
 import SelectCustom from '../../components/Custom/SelectCustom';
+import Header from '../../components/Header';
+import { useState } from 'react';
+import PageLayout from '../../components/PageLayout';
+import NextFooter from '../../common/Footer/NextFooter';
+import { IcCancelDark } from '../../assets/icon';
+import { useNavigate } from 'react-router-dom';
 
 const SelectPage = () => {
+  const navigate = useNavigate();
+
+  const [isActiveNext, setIsActiveNext] = useState(false);
+
+  const renderSelectCustomPageHeader = () => {
+    return (
+      <Header
+        leftSection={<St.BlankSection></St.BlankSection>}
+        title='커스텀 타투'
+        rightSection={<IcCancelDark onClick={() => navigate('/onboarding')} />}
+      />
+    );
+  };
+
   return (
-    <>
+    <PageLayout
+      renderHeader={renderSelectCustomPageHeader}
+      footer={<NextFooter isActiveNext={isActiveNext} navigateURL='/custom-size' />}
+    >
       <SelectCustom />
-    </>
+    </PageLayout>
   );
 };
 
 export default SelectPage;
+
+const St = {
+  BlankSection: styled.div`
+    width: 2.4rem;
+    height: 2.4rem;
+    background-color: transparent;
+  `,
+};
