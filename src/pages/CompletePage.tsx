8b321b6cffa9269f418dfe7f92c0e49a44d067fd
lydiacho <sheecho0804@ewhain.net>
2023-07-16 21:55:55 +0900

    feat: 구분선 추가

--- src/pages/CompletePage.tsx
+++ src/pages/CompletePage.tsx
@@ -1,15 +1,31 @@
 import styled from 'styled-components';
 import Result from '../common/Result';
+import PageLayout from '../components/PageLayout';
+import Header from '../components/Header';
 
 const CompletePage = () => {
+  const renderCompletePageHeader = () => {
+    return <Header title='주문하기' />;
+  };
+
   return (
-    <div>
+    <PageLayout renderHeader={renderCompletePageHeader}>
       <Result
         mainText={'결제가 완료되었어요'}
         description={'3일 내에 배송이 시작되며, 문자로 안내드려요'}
       />
-    </div>
+      <St.Line />
+    </PageLayout>
   );
 };
 
 export default CompletePage;
+
+const St = {
+  Line: styled.hr`
+    height: 1.3rem;
+
+    background-color: ${({ theme }) => theme.colors.bg};
+    border-width: 0rem;
+  `,
+};
