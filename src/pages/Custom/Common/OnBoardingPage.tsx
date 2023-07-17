a5d6d32d3563ea7b850000eeae98aa57cb47e5f2
Yeonseo-Jo <jys073190@gmail.com>
2023-07-17 20:54:14 +0900

    feat : 헤더 추가

--- src/pages/Custom/Common/OnBoardingPage.tsx
+++ src/pages/Custom/Common/OnBoardingPage.tsx
@@ -1,10 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import { IcCancelDark } from '../../../assets/icon';
 import OnBoardingCarousel from '../../../components/Custom/Common/OnBoardingCarousel';
+import Header from '../../../components/Header';
+import PageLayout from '../../../components/PageLayout';
 
 const OnBoardingPage = () => {
+  const navigate = useNavigate();
+
+  const renderOnBoardingPageHeader = () => {
+    return (
+      <Header title='커스텀 타투' rightSection={<IcCancelDark onClick={() => navigate('/')} />} />
+    );
+  };
+
   return (
-    <div>
+    <PageLayout renderHeader={renderOnBoardingPageHeader}>
       <OnBoardingCarousel />
-    </div>
+    </PageLayout>
   );
 };
 
