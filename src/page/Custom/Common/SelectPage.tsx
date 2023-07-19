10ae693fa7e4045a1db8e1301c0a695f5448ddb8
Yeonseo-Jo <jys073190@gmail.com>
2023-07-19 18:14:19 +0900

    chore : selectPage 파일 위치 수정

--- src/page/Custom/Common/SelectPage.tsx
+++ src/page/Custom/Common/SelectPage.tsx
@@ -1,12 +1,12 @@
 import { styled } from 'styled-components';
-import SelectCustom from '../../components/Custom/SelectCustom';
-import Header from '../../components/Header';
+import SelectCustom from '../../../components/Custom/SelectCustom';
+import Header from '../../../components/Header';
 import { useState } from 'react';
-import PageLayout from '../../components/PageLayout';
-import { IcCancelDark } from '../../assets/icon';
+import PageLayout from '../../../components/PageLayout';
+import { IcCancelDark } from '../../../assets/icon';
 import { useNavigate } from 'react-router-dom';
-import SelectCustomFooter from '../../components/Custom/SelectCustomFooter';
-import SelectCustomPolicy from '../../components/Custom/SelectCustomPolicy';
+import SelectCustomFooter from '../../../components/Custom/SelectCustomFooter';
+import SelectCustomPolicy from '../../../components/Custom/SelectCustomPolicy';
 
 const SelectPage = () => {
   const navigate = useNavigate();
