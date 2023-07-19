a22e77c5e4dfacd2e4c98faca993f26e45dadae5
seoAreum <1971236@hansung.ac.kr>
2023-07-19 15:19:53 +0900

    chore: 기존 사용자도 접속할 때마다 로그인 해야하는 옵션 삭제

--- src/constants/OAuth.ts
+++ src/constants/OAuth.ts
@@ -1,5 +1,4 @@
 const CLIENT_ID = import.meta.env.VITE_APP_REST_API_KEY;
 const REDIRECT_URI = import.meta.env.VITE_APP_REDIRECT_URI;
 
-// prompt=login: 기존 사용자 인증 여부와 상관없이, 로그인 화면 출력 (추가 설정/ 필수 아님!)
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&prompt=login`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
