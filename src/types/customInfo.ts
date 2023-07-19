3eb0cd0f13120030c15466dbce24f4ea7f181531
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-07-19 22:43:33 +0900

    feature: types에 custom에 쓰이는 request 내용 작성

---
+++ src/types/customInfo.ts
@@ -0,0 +1,15 @@
+export interface customInfo {
+  customId: number;
+  size: string;
+  isColored: boolean;
+  themes: number[];
+  styles: number[];
+  name: string;
+  description: string;
+  demand: string;
+  count: number;
+  isPublic: boolean;
+  isCompleted: boolean;
+  price: number;
+  viewCount: number;
+}
