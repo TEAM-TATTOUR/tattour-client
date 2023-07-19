07734b794eb25d335f5ae34e9140036c743d4a7d
lydiacho <sheecho0804@ewhain.net>
2023-07-19 20:22:19 +0900

    init: 스티커 필터링해서 조회-세팅

---
+++ src/libs/hooks/useGetAllList.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import api from '../api';
+import { AxiosError } from 'axios';
+
+export interface HotCustomItemProps {
+  id: number;
+  imageUrl: string;
+  name: string;
+  price: number;
+  discountPrice: number;
+  discountRate: number;
+  isCustom: boolean;
+}
+
+interface HotCustomResponse {
+  data: {
+    stickers: HotCustomItemProps[];
+  };
+  code: number;
+  message: string;
+}
+
+const useGetHotCustom = () => {
+  const [response, setResponse] = useState<HotCustomItemProps[]>([]);
+  const [error, setError] = useState<AxiosError>();
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    await api
+      .get('/stickers/custom/hot')
+      .then((res) => {
+        const data: HotCustomResponse = res.data;
+        setResponse(data.data.stickers);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { response, error, loading };
+};
+
+export default useGetHotCustom;
