d518247678464c8098dd7d6438b32af8e104ebb0
lydiacho <sheecho0804@ewhain.net>
2023-07-20 17:01:04 +0900

    fix: 안쓰는 useEffect 삭제

--- src/libs/hooks/order/usePostOrder.tsx
+++ src/libs/hooks/order/usePostOrder.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import api from '../../api';
 import { AxiosError } from 'axios';
 import { useNavigate } from 'react-router-dom';
