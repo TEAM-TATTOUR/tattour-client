70647e266e594e71b5e574c7c0e67aefe99b3965
Yeonseo-Jo <jys073190@gmail.com>
2023-07-20 16:02:22 +0900

    fix : price 페이지 state 없을 때 오류 발생 수정

--- src/page/Custom/PricePage.tsx
+++ src/page/Custom/PricePage.tsx
@@ -19,7 +19,11 @@ const PricePage = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const { haveDesign, customInfo: prevCustomInfo, customMainImage, customImages } = location.state;
+  const haveDesign = location.state ? location.state.haveDesign : null;
+  const prevCustomInfo = location.state ? location.state.customInfo : null;
+  const customMainImage = location.state ? location.state.customInfo : null;
+
+  // const { haveDesign, customInfo: prevCustomInfo, customMainImage, customImages } = location.state; //가져와야 할 값들 -> 확인 후 지워주세요!
 
   const CUSTOM_VIEW_COUNT = haveDesign ? 7 : 4;
   const backNavigateURL = haveDesign ? '/additional-request' : '/custom-request';
@@ -28,6 +32,8 @@ const PricePage = () => {
     ...prevCustomInfo,
     viewCount: CUSTOM_VIEW_COUNT,
   };
+
+  console.log(customInfo, customMainImage); //오류 발생 방지 용 console 나중에 footer로 넘겨주고 지워주세요!
 
   const renderPricePageHeader = () => {
     return (
