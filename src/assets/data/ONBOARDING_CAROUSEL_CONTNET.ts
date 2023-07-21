4d5687d3b0ef92a41908be843dbba0ed1e373975
Yeonseo-Jo <jys073190@gmail.com>
2023-07-22 08:26:38 +0900

    fix : 온보딩 캐러셀 수정

--- src/assets/data/ONBOARDING_CAROUSEL_CONTNET.ts
+++ src/assets/data/ONBOARDING_CAROUSEL_CONTNET.ts
@@ -1,33 +1,48 @@
-import onboarding_custom_1 from '../image/onboarding_custom_1.png';
-import onboarding_custom_2 from '../image/onboarding_custom_2.png';
-import onboarding_custom_3 from '../image/onboarding_custom_3.png';
-import onboarding_custom_4 from '../image/onboarding_custom_4.png';
-import onboarding_custom_5 from '../image/onboarding_custom_5.png';
+import onboarding_img_1 from '../image/onboarding_img_1.png';
+import onboarding_img_2 from '../image/onboarding_img_2.png';
+import onboarding_img_3 from '../image/onboarding_img_3.png';
+import onboarding_img_4 from '../image/onboarding_img_4.png';
+import onboarding_img_5 from '../image/onboarding_img_5.png';
 
 export const CUSTOM_CAROUSEL_DATA = [
   {
     id: 'page1',
-    content: onboarding_custom_1,
+    bannerText: '',
+    mainText: '도안은 이렇게 신청해주세요',
+    subText: '저작권 소유자가 분명한 경우에는 제작이 어려워요',
+    mockUpImg: onboarding_img_1,
     imgAlt: '도안-신청-온보딩-이미지',
   },
   {
     id: 'page2',
-    content: onboarding_custom_2,
+    bannerText: '',
+    mainText: '제작 방식을 선택해주세요',
+    subText: '내 도안 그대로 제작하거나, 도안을 의뢰할 수 있어요',
+    mockUpImg: onboarding_img_2,
     imgAlt: '제작-방식-선택-온보딩-이미지',
   },
   {
     id: 'page3',
-    content: onboarding_custom_3,
-    imgAlt: '그린-도안-제작-온보딩-이미지',
+    bannerText: '내 도안 그대로 만들기',
+    mainText: '내가 그린 도안으로 타투를 제작해요',
+    subText: '직접 그린 도안을 공개하면 즉시 할인 받을 수 있어요',
+    mockUpImg: onboarding_img_3,
+    imgAlt: '도안-그대로-만들기-온보딩-이미지',
   },
   {
     id: 'page4',
-    content: onboarding_custom_4,
+    bannerText: '타투어에게 도안 의뢰하기',
+    mainText: '원하는 도안 제작을 의뢰해요',
+    subText: '첨부 이미지와 간단 스케치를 참고해 제작해드려요',
+    mockUpImg: onboarding_img_4,
     imgAlt: '도안-제작-의뢰-온보딩-이미지',
   },
   {
     id: 'page5',
-    content: onboarding_custom_5,
+    bannerText: '타투어에게 도안 의뢰하기',
+    mainText: '제작 받은 타투어의 도안을 공개해요',
+    subText: '타투어 커스텀 도안을 공개하면 즉시 할인 받을 수 있어요',
+    mockUpImg: onboarding_img_5,
     imgAlt: '제작-도안-공개-온보딩-이미지',
   },
 ];
