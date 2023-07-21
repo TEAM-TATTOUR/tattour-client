621abad00a453d535c2327699c016b3b28dfef33
seoAreum <1971236@hansung.ac.kr>
2023-07-22 06:21:26 +0900

    refactor: 간격조정

--- src/components/Search/SearchResultItem.tsx
+++ src/components/Search/SearchResultItem.tsx
@@ -61,6 +61,7 @@ const St = {
   SearchResultPriceWrapper: styled.p`
     display: flex;
     margin-top: 0.4rem;
+    gap: 0.5rem;
   `,
 
   SearchResultItemDiscountRate: styled.span`
@@ -74,6 +75,8 @@ const St = {
   `,
 
   SearchResultItemOriginalPrice: styled.span`
+    margin-top: 0.3rem;
+
     ${({ theme }) => theme.fonts.body_line_medium_14};
     color: ${({ theme }) => theme.colors.gray1};
   `,
