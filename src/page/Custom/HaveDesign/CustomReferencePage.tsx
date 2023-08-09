67279d51e217410018384a580186ea0b38596646
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-08-09 22:22:03 +0900

    refactor: 불필요한 변수 제거 피드백 반영

--- src/page/Custom/HaveDesign/CustomReferencePage.tsx
+++ src/page/Custom/HaveDesign/CustomReferencePage.tsx
@@ -20,15 +20,9 @@ const CustomReferencePage = () => {
   const [isActiveNext, setIsActiveNext] = useState(false);
   // const [handDrawingImage, setHandDrawingImage] = useState<File | null>(null);
   // const [customImages, setCustomImages] = useState<FileList | null>(null);
-  const [, setFreeDraw] = useState<boolean>(location.state.freeDraw ? true : false);
-  // const [previewURL, setPreviewURL] = useState<string[]>([]); //페이지로 뺴주기
-
-  console.log('back', location.state.freeDraw);
 
   const haveDesign = location.state ? location.state.haveDesign : null;
   const prevCustomInfo = location.state ? location.state.customInfo : null;
-  // const attachedImages =
-  //   location.state && location.state.customImages ? location.state.customImages : null;
 
   useEffect(() => {
     if (!location.state) navigate('/onboarding');
@@ -75,11 +69,6 @@ const CustomReferencePage = () => {
         setIsActiveNext={setIsActiveNext}
         setHandDrawingImage={setHandDrawingImage}
         setCustomImages={setCustomImages}
-        customImages={customImages}
-        handDrawingImage={handDrawingImage}
-        setFreeDraw={setFreeDraw}
-        setPreviewURL={setPreviewURL}
-        previewURL={previewURL}
       />
     </PageLayout>
   );
