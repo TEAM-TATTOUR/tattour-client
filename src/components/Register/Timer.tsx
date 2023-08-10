52f140923c3528ae3f86a2f48de996fe0a392b09
seoAreum <1971236@hansung.ac.kr>
2023-08-11 02:14:53 +0900

    feat: memo 삭제 후, 렌더링 최적화를 위해 useRef 활용

--- src/components/Register/Timer.tsx
+++ src/components/Register/Timer.tsx
@@ -1,4 +1,4 @@
-import { memo, useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import { styled } from 'styled-components';
 import { reducer } from '../../libs/reducers/registerReducer';
 
@@ -7,7 +7,7 @@ interface TimerProps {
   setIsTimeout: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Timer = memo(({ isTimeout, setIsTimeout }: TimerProps) => {
+const Timer = ({ isTimeout, setIsTimeout }: TimerProps) => {
   const MINUTES_IN_MS = 5 * 60 * 1000;
   const INTERVAL = 1000;
 
@@ -17,25 +17,27 @@ const Timer = memo(({ isTimeout, setIsTimeout }: TimerProps) => {
     leftTime: MINUTES_IN_MS,
   });
 
+  const timer = useRef(0);
+
   // padStart(2, '0'): 문자열의 길이는 2로 하고, 빈 곳이 있으면 0으로 채워넣어줌.
   const minutes = String(Math.floor((state.leftTime / (1000 * 60)) % 60)).padStart(2, '0');
   const second = String(Math.floor((state.leftTime / 1000) % 60)).padStart(2, '0');
 
   useEffect(() => {
     // setInterval: 두 번째 인자(시간(ms))를 간격으로 첫 번째 인자(코드) 실행
-    const timer = setInterval(() => {
+    timer.current = setInterval(() => {
       // 1초씩 감소
       dispatch({ type: 'SET_LEFT_TIME', payload: INTERVAL });
     }, INTERVAL);
 
     if (state.leftTime <= 0) {
       // timer 반복 중단
-      clearInterval(timer);
+      clearInterval(timer.current);
       setIsTimeout(true);
     }
 
     return () => {
-      clearInterval(timer);
+      clearInterval(timer.current);
     };
   }, [state.leftTime, setIsTimeout]);
 
@@ -44,7 +46,7 @@ const Timer = memo(({ isTimeout, setIsTimeout }: TimerProps) => {
       {isTimeout ? '시간 만료' : `${minutes} : ${second}`}
     </St.AuthTimer>
   );
-});
+};
 
 const St = {
   AuthTimer: styled.span<{ $isTimeout: boolean }>`
