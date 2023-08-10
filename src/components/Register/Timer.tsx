571ab6b43f639c9ae5dfcd1e0f74d6fc8ecbd084
seoAreum <1971236@hansung.ac.kr>
2023-08-10 18:27:47 +0900

    feat: reducer로 state 대체

--- src/components/Register/Timer.tsx
+++ src/components/Register/Timer.tsx
@@ -1,28 +1,34 @@
-import { memo } from 'react';
-import { useEffect } from 'react';
+import { memo, useEffect, useReducer } from 'react';
 import { styled } from 'styled-components';
+import { reducer } from './RegisterPhoneNumForm';
 
 interface TimerProps {
   isTimeout: boolean;
   setIsTimeout: React.Dispatch<React.SetStateAction<boolean>>;
-  leftTime: number;
-  setLeftTime: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const Timer = memo(({ isTimeout, setIsTimeout, leftTime, setLeftTime }: TimerProps) => {
+const Timer = memo(({ isTimeout, setIsTimeout }: TimerProps) => {
+  const MINUTES_IN_MS = 5 * 60 * 1000;
   const INTERVAL = 1000;
+
+  const [state, dispatch] = useReducer(reducer, {
+    isVisible: false,
+    isError: false,
+    leftTime: MINUTES_IN_MS,
+  });
+
   // padStart(2, '0'): 문자열의 길이는 2로 하고, 빈 곳이 있으면 0으로 채워넣어줌.
-  const minutes = String(Math.floor((leftTime / (1000 * 60)) % 60)).padStart(2, '0');
-  const second = String(Math.floor((leftTime / 1000) % 60)).padStart(2, '0');
+  const minutes = String(Math.floor((state.leftTime / (1000 * 60)) % 60)).padStart(2, '0');
+  const second = String(Math.floor((state.leftTime / 1000) % 60)).padStart(2, '0');
 
   useEffect(() => {
     // setInterval: 두 번째 인자(시간(ms))를 간격으로 첫 번째 인자(코드) 실행
     const timer = setInterval(() => {
       // 1초씩 감소
-      setLeftTime((prevTime: number) => prevTime - INTERVAL);
+      dispatch({ type: 'SET_LEFT_TIME', payload: INTERVAL });
     }, INTERVAL);
 
-    if (leftTime <= 0) {
+    if (state.leftTime <= 0) {
       // timer 반복 중단
       clearInterval(timer);
       setIsTimeout(true);
@@ -31,7 +37,7 @@ const Timer = memo(({ isTimeout, setIsTimeout, leftTime, setLeftTime }: TimerPro
     return () => {
       clearInterval(timer);
     };
-  }, [leftTime, setIsTimeout]);
+  }, [state.leftTime, setIsTimeout]);
 
   return (
     <St.AuthTimer $isTimeout={isTimeout}>
