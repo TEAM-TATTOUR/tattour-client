7c240a82bf79561d75e73ab645cb0b024d39442c
lydiacho <sheecho0804@ewhain.net>
2023-10-09 23:37:14 +0900

    fix: 내타투 좋아요 아이템클릭시 오류 해결

--- src/libs/hooks/useGetLikeSticker.ts
+++ src/libs/hooks/useGetLikeSticker.ts
@@ -1,50 +1,49 @@
-import { AxiosError } from "axios";
-import api from "../api";
-import { useState, useEffect } from "react";
+import { AxiosError } from 'axios';
+import api from '../api';
+import { useState, useEffect } from 'react';
 
 export interface LikeStickerProps {
-    id: number;
-    name: string;
-    price: number;
-    discountRate: number;
-    discountPrice: number;
-    mainImageUrl: string;
+  stickerId: number;
+  name: string;
+  price: number;
+  discountRate: number;
+  discountPrice: number;
+  mainImageUrl: string;
 }
 
 interface LikeStickerResponse {
-    data: {
-        stickersliked: LikeStickerProps[];
-    };
-    code: number;
-    message: string;
+  data: {
+    stickersliked: LikeStickerProps[];
+  };
+  code: number;
+  message: string;
 }
 
 const useGetLikeSticker = () => {
-    const [response, setResponse] = useState<LikeStickerProps[]>([]);
-    const [error, setError] = useState<AxiosError>();
-    const [loading, setLoading] = useState(true);
+  const [response, setResponse] = useState<LikeStickerProps[]>([]);
+  const [error, setError] = useState<AxiosError>();
+  const [loading, setLoading] = useState(true);
 
-    const fetchData = async () => {
-        await api.get('/user/productliked/saved')
-            .then(res => {
-                const data: LikeStickerResponse = res.data;
-                setResponse(data.data.stickersliked);
-            })
-            .catch(err => {
-                setError(err);
-            })
-            .finally(() => {
-                setLoading(false);
-            }
-            );
-    }
+  const fetchData = async () => {
+    await api
+      .get('/user/productliked/saved')
+      .then((res) => {
+        const data: LikeStickerResponse = res.data;
+        setResponse(data.data.stickersliked);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    return { response, error, loading, fetchData };
-}
+  return { response, error, loading, fetchData };
+};
 
 export default useGetLikeSticker;
-
