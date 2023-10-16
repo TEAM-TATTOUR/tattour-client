d9c99cc8100769377e34f0639ab4b963a1edcdd2
lydiacho <sheecho0804@ewhain.net>
2023-10-16 20:16:22 +0900

    install: react-lottie TS 버전 설치

---
+++ src/page/LoadingPage.tsx
@@ -0,0 +1,8 @@
+import Lottie from 'react-lottie';
+import * as animationData from './pinjump.json';
+
+const LoadingPage = () => {
+  return <div>LoadingPage</div>;
+};
+
+export default LoadingPage;
