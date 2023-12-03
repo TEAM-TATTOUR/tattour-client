65cda369205014168199abb62899e953bdfdc97b
Yeonseo-Jo <jys073190@gmail.com>
2023-12-03 16:22:34 +0900

    feat : 무통장 입금 안내 문구 추상화, 커스텀 무통장 입금 예시로 적용

--- src/page/Custom/Common/CustomDirectDepositPage.tsx
+++ src/page/Custom/Common/CustomDirectDepositPage.tsx
@@ -1,15 +1,26 @@
-import AccountCopy from '../../../components/DirectDeposit/AccountCopy';
-import DepositInfo from '../../../components/DirectDeposit/DepositInfo';
+import styled from 'styled-components';
+import DepositInfo from '../../../components/DirectDeposit/Common/DepositInfo';
+import AccountCopy from '../../../components/DirectDeposit/Common/AccountCopy';
+import CustomDepositInfo from '../../../components/DirectDeposit/Custom/CustomDepositInfo';
 
 const CustomDirectDepositPage = () => {
   return (
-    <>
+    <St.CustomDirectDepositPageWrapper>
       <DepositInfo chargeAmount={5000}>
-        <div>신청서를~</div>
+        <CustomDepositInfo />
       </DepositInfo>
       <AccountCopy />
-    </>
+    </St.CustomDirectDepositPageWrapper>
   );
 };
 
 export default CustomDirectDepositPage;
+
+const St = {
+  CustomDirectDepositPageWrapper: styled.main`
+    display: flex;
+    flex-direction: column;
+
+    padding: 0 2rem;
+  `,
+};
