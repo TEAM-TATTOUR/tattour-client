6b0320ad508b3fb3cffaaea0f3a66834efae3639
Yeonseo-Jo <jys073190@gmail.com>
2023-12-03 16:16:18 +0900

    fix : DespoitInfo 주석 제거

--- src/components/DirectDeposit/DepositInfo.tsx
+++ src/components/DirectDeposit/DepositInfo.tsx
@@ -9,12 +9,6 @@ const DepositInfo = ({ chargeAmount, children }: DepositInfoProps) => {
   return (
     <St.TransferMainWrapper>
       <St.TransferInfoContainer>
-        {/* <St.InfoMsgHeader>
-          <St.InfoMsgTitle>아래 계좌로 금액을 송금해주세요</St.InfoMsgTitle>
-          <St.InfoMsgDetail>
-            정확하게 송금하지 않을 시 추후에 주문이 취소될 수 있어요
-          </St.InfoMsgDetail>
-        </St.InfoMsgHeader> */}
         {children}
         <St.InfoPriceMain>
           <St.InfoPriceLeft>결제 금액</St.InfoPriceLeft>
@@ -40,26 +34,6 @@ const St = {
   TransferInfoContainer: styled.article`
     display: flex;
     flex-direction: column;
-
-    padding-top: 6rem;
-  `,
-
-  InfoMsgHeader: styled.header`
-    display: flex;
-    flex-direction: column;
-    gap: 1.2rem;
-
-    width: 100%;
-  `,
-
-  InfoMsgTitle: styled.h2`
-    color: ${({ theme }) => theme.colors.gray8};
-    ${({ theme }) => theme.fonts.title_semibold_20};
-  `,
-
-  InfoMsgDetail: styled.p`
-    color: ${({ theme }) => theme.colors.gray3};
-    ${({ theme }) => theme.fonts.body_medium_14};
   `,
 
   InfoPriceMain: styled.main`
@@ -67,7 +41,7 @@ const St = {
     justify-content: space-between;
     align-items: center;
 
-    margin: 3.4rem 0 rem 0;
+    margin: 3.4rem 0 3.7rem 0;
   `,
 
   InfoPriceLeft: styled.p`
