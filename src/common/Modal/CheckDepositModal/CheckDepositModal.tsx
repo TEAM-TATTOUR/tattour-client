07e9b443d98680130e930bd62cf78e8caed1e5ba
seoAreum <1971236@hansung.ac.kr>
2023-12-07 17:21:47 +0900

    feat: 무통장 입금 확인 모달 생성

---
+++ src/common/Modal/CheckDepositModal/CheckDepositModal.tsx
@@ -0,0 +1,26 @@
+import ModalPortal from '../ModalPortal';
+import EscapeModalForm from '../EscapeModal/EscapeModalForm';
+
+interface CheckDepositModalProps {
+  setModalOn: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsActiveNext: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const CheckDepositModal = ({ setModalOn, setIsActiveNext }: CheckDepositModalProps) => {
+  return (
+    <ModalPortal>
+      <EscapeModalForm
+        onClose={() => setModalOn(false)}
+        pageName={'OrderPage'}
+        title={'입금을 완료하셨나요?'}
+        subTitle={'입금을 하지 않으면 주문이 반려될 수 있어요'}
+        subTitle2={'해당 계좌로 입금을 진행해주세요'}
+        continueBtn={'입금했어요'}
+        stopBtn={'돌아가기'}
+        setIsActiveNext={setIsActiveNext}
+      />
+    </ModalPortal>
+  );
+};
+
+export default CheckDepositModal;
